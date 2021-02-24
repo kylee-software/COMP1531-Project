8dadb24f72f3d351d78aa68781733161081191ec
Sesi Adams <Sesi@sesis-mbp.home>
2021-02-24 13:31:11 +1100

    Comments about testing functions

---
+++ tests/auth_login_v1_test.py
@@ -0,0 +1,9 @@
+# Does the given email belong to a registered user? - Even if valid if not for a registered user - input error 
+# 1. If the email belongs to registered user - fine
+# 2. if the email does not belong to a registered user - input error
+# Is the email given a valid email? - Follows the regular expression given in section 3.3. - otherwise input error
+#  3. if the email is a valid email - fine
+#  4. if the email is not a valid email - inputerror 
+# If the password is found in the system does it belong to the user? - input error 
+#  5. If the email belongs to the user given via email - fine
+#  6.if the email does not belong to the user given via email - input error
