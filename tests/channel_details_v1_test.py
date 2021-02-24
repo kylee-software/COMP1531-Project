8ea8c586a080afde75a8599febfcc315785b8d42
Isabelle Ryan <z5205211@ad.unsw.edu.au>
2021-02-24 12:56:09 +1100

    Made rough outline of test cases

---
+++ tests/channel_details_v1_test.py
@@ -0,0 +1,14 @@
+
+import pytest
+
+# InputError: channel_id is not valid
+# want to check channel_id that does not exist
+# want to check channel_id that does exist
+
+# Other error: user id doesnt exist --> should be Input I think
+# check using an existing id
+# check using a not existing id
+
+# AccessError: auth_id is not part of channel
+# want to check auth_id is in channel
+# check auth_id that is not in channel
