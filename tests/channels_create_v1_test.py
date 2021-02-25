6785c0ed3bc7319419b8c0bfbd06b4ec1a0aa574
Kylee Bowers <k.bowers@student.unsw.edu.au>
2021-02-25 15:01:22 +1100

    Added rough test draft for channels_create_v1

---
+++ tests/channels_create_v1_test.py
@@ -0,0 +1,9 @@
+# import pytest
+# from src.channels import channels_create_v1
+
+def test_name():
+    # Test invalid name with more tan 20 characters --> "InputError"
+    # Test valid name
+    pass
+   
+
