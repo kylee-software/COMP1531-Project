5d87b8a57c75ab57e61713420b4a8f2d76ea835f
Isabelle Ryan <z5205211@ad.unsw.edu.au>
2021-02-28 20:28:39 +1100

    planned out a little more of the tests - realised channel_join would be better to work on first

--- tests/channel_details_v1_test.py
+++ tests/channel_details_v1_test.py
@@ -1,13 +1,35 @@
-
+import users, channels from data.py
 import pytest
 
-# InputError: channel_id is not valid
-# want to check channel_id that does not exist
-# want to check channel_id that does exist
+@pytest.fixture
+def call_clear():
+    # call the clear function from the project to reset all data
+    clear_v1()
 
-# Other error: user id doesnt exist --> should be Input I think
+@pytet.fixture
+def create_user():
+    # create a test user and return the id
+    return auth_register_v1("testemail@gmail.com", "TestPassword1", "firstname", "lastname")
+
+@pytest.fixture
+def create_channel(user_id, is_public):
+    # create a test channel and return the id
+    return channel_create_v1(user_id, "TestChannel", is_public)
+
+
+def test_channel_id():
+    # want to check channel_id that does not exist - InputError
+    assert
+    # want to check channel_id that does exist
+    assert 
+
+
+def test_user_existance():
+# User not exist error: user id doesnt exist --> should be Input I think
 # check using an existing id
 # check using a not existing id
+
+def test_user_access():
 
 # AccessError: auth_id is not part of channel
 # want to check auth_id is in channel
