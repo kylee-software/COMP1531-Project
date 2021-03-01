60e18df6b10157597ecbf22bf11f741e4558d4fe
Sesi Adams <Sesi@sesis-mbp.home>
2021-03-01 21:38:46 +1100

    Test functions for inputerrors and comments/notes on tests for handle

--- tests/auth_register_v1_test.py
+++ tests/auth_register_v1_test.py
@@ -1,2 +1,25 @@
-import pytest 
+import pytest
 
+# Test exception - Email entered is not valid (wrong format)
+def test_given_email_is_invalid():
+    pass 
+
+# Test exception - Email entered already exists (belongs to an existing user)
+def test_email_already_exists():
+    pass 
+
+# Test exception - Password given is less than 6 Characters
+def test_password_incorrect_length():
+    pass 
+
+# Test exception - First name given is between 1 and 50 inclusive 
+def test_first_name_valid_length():
+    pass 
+
+# Test exception - Last name given is between 1 and 50 inclusive 
+def test_last_name_valid_length():
+    pass 
+
+# Tests for handle - no @ or whitespace, 20 characters only, Q - in the case where we need to add numbers to handles as the handle already exists, is it allowed to go over the 20 character limiit?
+# Need clarification on names and handles - special characters, are @ and whitespace allowed in names, are special characters like #, _, -, $, % or \ allowed in names and handles? 
+# Are numbers allowed in first or last names and are numbers in names given allowed in handles?
