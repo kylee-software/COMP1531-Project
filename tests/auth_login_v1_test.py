c6aaf7b3f31c2dd629c164f9c1e0c43414d65a62
Sesi Adams <Sesi@sesis-mbp.home>
2021-03-02 00:00:37 +1100

    Started writing implementation for auth_login_v1_test.py

--- tests/auth_login_v1_test.py
+++ tests/auth_login_v1_test.py
@@ -2,16 +2,17 @@ import pytest
 
 # Test exception - Email given is not valid (wrong format)
 def test_given_invalid_email(): 
-    pass 
+    with pytest.raises(InputError) 
 
 # Test exception - Email given doesn't match a user's email (email doesn't exist)
 def test_given_email_nonexistent():
-    pass 
+    with pytest.raises(InputError)
 
 # Test exception - password given is not correct
 def test_password_incorrect():
-    pass 
+    with pytest.raises(InputError)
 
 # Test where auth_user_id given as email and password given are correct
 def correct_login_details_given(): 
-    pass  
+    with pytest.raises(InputError)
+     
