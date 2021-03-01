a19fba7ae1a7c3251f44a1aa19e6016c38137e9b
Kylee Bowers <k.bowers@student.unsw.edu.au>
2021-03-01 17:37:48 -0600

    Updated test file for channels_create_v1 function. Theses are all the test cases I could think of atm.

--- tests/channels_create_v1_test.py
+++ tests/channels_create_v1_test.py
@@ -1,9 +1,32 @@
-# import pytest
-# from src.channels import channels_create_v1
+import pytest
+from src.other import clear_v1
+from src.auth import auth_register_v1
+from src.channels import channels_create_v1
 
-def test_name():
+@pytest.fixure
+def call_clear():
+    # rest the all data
+    clear_v1()
+
+@pytest.fixture
+def create_user():
+    # create a test user and return auth id
+    return auth_register_v1("testmail@gamil.com", "Testpass12345", "firstname", "lastname")
+
+
+def test_invalid_name():
     # Test invalid name with more tan 20 characters --> "InputError"
-    # Test valid name
-    pass
-   
+    assert channels_create_v1(create_user, "fffffffffffffffffffff", "is_public") == "Name is more than 20 characters " \
+                                                                                    "long! "
+
+
+def test_valid_name_public():
+    # Given a valid name and is_public set to true, assert that the return value channel_id is an integer
+    assert type(channels_create_v1(create_user, "channelName1", True)) is int
+
+
+def test_valid_name_private():
+    # Given a valid name and is_public set to false, assert that the return value channel_id is an integer
+    assert type(channels_create_v1(create_user, "channelName2", False)) is int
+
 
