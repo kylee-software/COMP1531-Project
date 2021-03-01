23eea46a096dfc8ba44970994117cc1945c71e92
Sesi Adams <Sesi@sesis-mbp.home>
2021-03-01 20:13:22 +1100

    Test functions and comments - errors fixed

--- tests/auth_login_v1_test.py
+++ tests/auth_login_v1_test.py
@@ -2,13 +2,16 @@ import pytest
 
 # Test exception - Email given is not valid (wrong format)
 def test_given_invalid_email(): 
+    pass 
 
 # Test exception - Email given doesn't match a user's email (email doesn't exist)
 def test_given_email_nonexistent():
+    pass 
 
 # Test exception - password given is not correct
 def test_password_incorrect():
+    pass 
 
 # Test where auth_user_id given as email and password given are correct
 def correct_login_details_given(): 
-
+    pass  
