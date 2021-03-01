6a3cda820f3766f47b750a75ed117c7ce0b14128
Sesi Adams <Sesi@sesis-mbp.home>
2021-03-01 20:09:39 +1100

    Test functions with comments outlining their purpose

--- tests/auth_login_v1_test.py
+++ tests/auth_login_v1_test.py
@@ -1,9 +1,14 @@
-# Does the given email belong to a registered user? - Even if valid if not for a registered user - input error 
-# 1. If the email belongs to registered user - fine
-# 2. if the email does not belong to a registered user - input error
-# Is the email given a valid email? - Follows the regular expression given in section 3.3. - otherwise input error
-#  3. if the email is a valid email - fine
-#  4. if the email is not a valid email - inputerror 
-# If the password is found in the system does it belong to the user? - input error 
-#  5. If the email belongs to the user given via email - fine
-#  6.if the email does not belong to the user given via email - input error
+import pytest 
+
+# Test exception - Email given is not valid (wrong format)
+def test_given_invalid_email(): 
+
+# Test exception - Email given doesn't match a user's email (email doesn't exist)
+def test_given_email_nonexistent():
+
+# Test exception - password given is not correct
+def test_password_incorrect():
+
+# Test where auth_user_id given as email and password given are correct
+def correct_login_details_given(): 
+
