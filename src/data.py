aea5250a1f7911d3c8764324a11c160c102d1130
Kylee Bowers <k.bowers@student.unsw.edu.au>
2021-03-02 00:36:42 -0600

    Updated data.py

--- src/data.py
+++ src/data.py
@@ -1,29 +1,35 @@
 # Just a draft of the layout, variable types are incorrect at the moment
+user_info = [{'user_id': 'number1',
+              'first_name': 'string',
+              'last_name': 'string',
+              'email': 'string',
+              'password': 'string',
+              'global_owner_status':'boolean',
+              'handle': 'string'},
+             {'user_id': 'number1',
+              'first_name': 'string',
+              'last_name': 'string',
+              'email': 'string',
+              'password': 'string',
+              'global_owner_status': 'boolean',
+              'handle': 'string',
+              }
+             ]
 
-register_info = {'email1@gmail.com': {'password': 'string1',
-                                      'user_id': 'integers'},
-                 'email2@gmail.com': {'password': 'string2',
-                                      'user_id': 'integers'}
+channel_info = [{'channel_id': 'id1',
+                 'name': 'string',
+                 'public_status': 'boolean',
+                 'members': [{'user_id': 'id1',
+                              'channel_owner_status': 'boolean'
+                              }],
+                 'messages': 'words'},
+                {'channel_id': 'id1',
+                 'name': 'string',
+                 'public_status': 'boolean',
+                 'members': [{'user_id': 'id1',
+                              'channel_owner_status': 'boolean'
+                              }],
+                 'messages': 'words'
                  }
-
-# user_ids are integers
-user_info = {'user_id_1': {'first_name': 'first_name',
-                           'last_name': 'last_name',
-                           'handle': 'string',
-                           'global_owner_status': 'boolean'},
-             'user_id_2': {'first_name': 'first_name',
-                           'last_name': 'last_name',
-                           'handle': 'string',
-                           'global_owner_status': 'boolean'}
-             }
-
-# user_ids are integers
-channel_info = {'channel_id_1': {'name': 'channel_name',
-                                 'members': {'user_id_1', 'user_id_2', 'user_id_3'},
-                                 'messages': 'string'},
-                'channel_id_2': {'name': 'channel_name',
-                                 'members': {'user_id_1', 'user_id_2', 'user_id_3'},
-                                 'messages': 'string'}
-                }
-
+                ]
 
