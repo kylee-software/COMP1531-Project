8f143a037c7f239ec9b4e992423be78c38d37995
Sesi Adams <Sesi@sesis-mbp.home>
2021-03-03 11:52:56 +1100

    Changes made after group discussion in lab

--- tests/auth_register_v1_test.py
+++ tests/auth_register_v1_test.py
@@ -1,4 +1,5 @@
 import pytest
+# import function import clear write fixtures 
 
 # Test exception - Email entered is not valid (wrong format)
 def test_given_email_is_invalid():
@@ -20,19 +21,15 @@ def test_first_name_valid_length():
 def test_last_name_valid_length():
     pass 
 
-# Test if email stored correctly in dictionary 
-def test_email_stored_in_dictionary():
-    pass 
-
-# Test if first name is stored correctly in dictionary
+# Test if first name is stored correctly 
 def test_first_name_stored_in_dictionary():
     pass 
 
-# Test if last name is stored correctly in dictionary
+# Test if last name is stored correctly 
 def test_if_last_name_stored_in_dictionary():
     pass 
 
-# Test if password is stored correctly in dictionary 
+# Test if password is stored correctly 
 def test_password_stored_in_dictionary():
     pass 
 
@@ -40,3 +37,4 @@ def test_password_stored_in_dictionary():
 # Tests for handle - no @ or whitespace, 20 characters only, Q - in the case where we need to add numbers to handles as the handle already exists, is it allowed to go over the 20 character limiit?
 # Need clarification on names and handles - special characters, are @ and whitespace allowed in names, are special characters like #, _, -, $, % or \ allowed in names and handles? 
 # Are numbers allowed in first or last names and are numbers in names given allowed in handles?
+# test for same name multiple times and then 
