e834092ddda63d75d124fd566f963d4dbc3e55d6
Kylee Bowers <k.bowers@student.unsw.edu.au>
2021-03-02 19:55:59 -0600

    Updated channels_create_v1 function

--- src/channels.py
+++ src/channels.py
@@ -1,4 +1,4 @@
-from data import data  # to add new channel_id and its corresponding data info to the dictionary
+import data # to add new channel_id and its corresponding data info to the dictionary
 from error import InputError # to handle InputError
 
 def channels_list_v1(auth_user_id):
@@ -29,16 +29,16 @@ def channels_create_v1(auth_user_id, name, is_public):
     Get the number of channels already exist to help to create the channel_id,
     i.e, channel_size + 1 = channel_id
     '''
-    channel_id = len(data['channels']) + 1
+    channel_id = len(data.data['channels']) + 1
 
     # Add the new channel info to the data
-    data['channels'][channel_id]['channel_id'] = channel_id
-    data['channels'][channel_id]['name'] = name
-    data['channels'][channel_id]['public_status'] = is_public
+    data.data['channels'][channel_id]['channel_id'] = channel_id
+    data.data['channels'][channel_id]['name'] = name
+    data.data['channels'][channel_id]['public_status'] = is_public
 
     # Add owner info to members list
-    data['channels'][channel_id]['members'][0]['user_id'] = auth_user_id
-    data['channels'][channel_id]['members'][0]['channel_owner_status'] = True
+    data.data['channels'][channel_id]['members'][0]['user_id'] = auth_user_id
+    data.data['channels'][channel_id]['members'][0]['channel_owner_status'] = True
 
     '''
     Add messages --> might have to import channel_messages_v1 from channel
