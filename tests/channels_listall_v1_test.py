4e4f2d991c81ee3a85088aceb479e4355d7c545f
Michael <michael@marmic.net.au>
2021-03-03 12:07:25 +1100

    added some tests

--- tests/channels_listall_v1_test.py
+++ tests/channels_listall_v1_test.py
@@ -10,139 +10,40 @@ from src.auth import auth_register_v1
 
 ##create one channel with one member and
 #  no messages and
-def test_oneChannel_oneMember_noMessages(auth_user_id):
+def test_oneChannel_oneMember_noMessages():
     clear_v1()
     auth_user_id = auth_register_v1("test@test.unsw.com", 'testPassword8', 'Test', "User")
     channels_create_v1(auth_user_id, 'testChannel01', False)
     assert len(channels_listall_v1(auth_user_id)) == 1
-    """assert channels_listall_v1(auth_user_id) == [ {'name': "testChannel01",
-                                                            'channel_id': 1,
-                                                            'public_status': False,
-                                                            'members' : [ { 'user_id': auth_user_id, 
-                                                                            'channel_owner_status': True,
-                                                                        },],   
-                                                            'messages':[],
-                                                            }]"""
-
+    
 ##test if there are no channels
-def test_noChannel(auth_user_id):
+def test_noChannel():
+    auth_user_id = auth_register_v1("test@test.unsw.com", 'testPassword8', 'Test', "User")
     clear_v1()
     assert channels_listall_v1(auth_user_id) == []
 
 ##test with multiple channels and public to true
 ##test with multiple channels 
-def test_fiveChannels_oneMember_noMessages_public(auth_user_id):
+def test_fiveChannels_oneMember_noMessages_public():
     clear_v1()
     auth_user_id = auth_register_v1("test@test.unsw.com", 'testPassword8', 'Test', "User")
     names = ['testChannel01', 'testChannel02', 'testChannel03', 'testChannel04', 'testChannel05']
     for name in names:
         channels_create_v1(auth_user_id, name, True)
     assert len(channels_listall_v1(auth_user_id)) == 5
-    """assert channels_listall_v1(auth_user_id) == [ {'name': "testChannel01",
-                                                            'channel_id': 1,
-                                                            'public_status': True,
-                                                            'members' : [ { 'user_id': auth_user_id, 
-                                                                            'channel_owner_status': True,
-                                                                        },],   
-                                                            'messages':[],
-                                                            },
-
-                                                        {'name': "testChannel02",
-                                                            'channel_id': 2,
-                                                            'public_status': True,
-                                                            'members' : [ { 'user_id': auth_user_id, 
-                                                                            'channel_owner_status': True,
-                                                                        },],   
-                                                            'messages':[],
-                                                            },
-
-                                                        {'name': "testChannel03",
-                                                            'channel_id': 3,
-                                                            'public_status': True,
-                                                            'members' : [ { 'user_id': auth_user_id, 
-                                                                            'channel_owner_status': True,
-                                                                        },],   
-                                                            'messages':[],
-                                                            },
-
-                                                        {'name': "testChannel04",
-                                                            'channel_id': 4,
-                                                            'public_status': True,
-                                                            'members' : [ { 'user_id': auth_user_id, 
-                                                                            'channel_owner_status': True,
-                                                                        },],   
-                                                            'messages':[],
-                                                            },
-
-                                                        {'name': "testChannel05",
-                                                            'channel_id': 5,
-                                                            'public_status': True,
-                                                            'members' : [ { 'user_id': auth_user_id, 
-                                                                            'channel_owner_status': True,
-                                                                        },],   
-                                                            'messages':[],
-                                                            },
-                                                            
-                                                        ]"""
-
+    
 ##test with multiple channels 
-def test_fiveChannels_oneMember_noMessages(auth_user_id):
+def test_fiveChannels_oneMember_noMessages():
     clear_v1()
     auth_user_id = auth_register_v1("test@test.unsw.com", 'testPassword8', 'Test', "User")
     names = ['testChannel01', 'testChannel02', 'testChannel03', 'testChannel04', 'testChannel05']
     for values in names:
         channels_create_v1(auth_user_id, values, False)
     assert len(channels_listall_v1(auth_user_id)) == 5
-    """assert channels_listall_v1(auth_user_id) == [ {'name': "testChannel01",
-                                                            'channel_id': 1,
-                                                            'public_status': False,
-                                                            'members' : [ { 'user_id': auth_user_id, 
-                                                                            'channel_owner_status': True,
-                                                                        },],   
-                                                            'messages':[],
-                                                            },
 
-                                                        {'name': "testChannel02",
-                                                            'channel_id': 2,
-                                                            'public_status': False,
-                                                            'members' : [ { 'user_id': auth_user_id, 
-                                                                            'channel_owner_status': True,
-                                                                        },],   
-                                                            'messages':[],
-                                                            },
-
-                                                        {'name': "testChannel03",
-                                                            'channel_id': 3,
-                                                            'public_status': False,
-                                                            'members' : [ { 'user_id': auth_user_id, 
-                                                                            'channel_owner_status': True,
-                                                                        },],   
-                                                            'messages':[],
-                                                            },
-
-                                                        {'name': "testChannel04",
-                                                            'channel_id': 4,
-                                                            'public_status': False,
-                                                            'members' : [ { 'user_id': auth_user_id, 
-                                                                            'channel_owner_status': True,
-                                                                        },],   
-                                                            'messages':[],
-                                                            },
-
-                                                        {'name': "testChannel05",
-                                                            'channel_id': 5,
-                                                            'public_status': False,
-                                                            'members' : [ { 'user_id': auth_user_id, 
-                                                                            'channel_owner_status': True,
-                                                                        },],   
-                                                            'messages':[],
-                                                            },
-                                                            
-                                                        ]"""
-                                                            
-                                                            
-
-
-
-
-
+def test_invalid_authId():
+    clear_v1()
+    auth_user_id = 4
+    with pytest.raise(InputError):
+        channels_listall_v1(auth_user_id)
+    
