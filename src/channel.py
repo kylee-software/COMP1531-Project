d6f8ccfc53b97001b2d731e602b1f8c600fda2a8
Michael Kappel <z1567704@unsw.edu.au>
2021-03-05 14:14:12 +1100

    fixed some erros

--- src/channel.py
+++ src/channel.py
@@ -45,42 +45,9 @@ def channel_leave_v1(auth_user_id, channel_id):
     }
 
 def channel_join_v1(auth_user_id, channel_id):
-    # First check will be to make sure channel_id is valid
-    found_channel_id = False
-    for channel in data['channels']:
-        if channel['channel_id'] == channel_id:
-            found_channel_id == True
-            break
-    if found_channel_id == False:
-        raise InputError
-    
-    # Next we find out if the auth_user_id user is a global owner
-    global_status = False
-    for user in data['users']:
-        if user['user_id'] == auth_user_id:
-            global_status == user['global_owner_status']
-            break
-    
-    for channel in data['channels']:
-        if channel['channel_id'] == channel_id:
-            # Now we check to see if the user is already in the channel
-            for member in channel['members']:
-                if member['user_id'] == auth_user_id:
-                    # member is already in channel so return as successful
-                    return {}
-            is_public = channel['public_status']
-
-            # Now if the user is a global owner or the channel is public they can be added
-            if global_status == True or is_public == True:
-                user_dict = {'user_id':auth_user_id, 'channel_owner_status':global_status,}
-                channel['members'].append(user_dict)
-            else:
-                # If not this means the channel is private and the user doesn't have access
-                raise AccessError
-            break
-    
     return {
     }
+
 
 def channel_addowner_v1(auth_user_id, channel_id, u_id):
     return {
