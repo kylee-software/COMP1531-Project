dcdaab2af6fe7389c79461557a3921811d9fe905
Isabelle Ryan <z5205211@ad.unsw.edu.au>
2021-03-05 13:50:56 +1100

    implemented channel.py

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,8 @@
+from src.data import data
+from src.error import AccessError, InputError
+
+global data
+
 def channel_invite_v1(auth_user_id, channel_id, u_id):
     return {
     }
@@ -40,6 +45,35 @@ def channel_leave_v1(auth_user_id, channel_id):
     }
 
 def channel_join_v1(auth_user_id, channel_id):
+    found_channel_id = False
+    for channel in data['channels']:
+        if channel['channel_id'] == channel_id:
+            found_channel_id == True
+            break
+    if found_channel_id == False:
+        raise InputError
+    
+    global_status = False
+    for user in data['users']:
+        if user['user_id'] == auth_user_id:
+            global_status == user['global_owner_status']
+            break
+    
+    for channel in data['channels']:
+        if channel['channel_id'] == channel_id:
+            for member in channel['members']:
+                if member['user_id'] == auth_user_id:
+                    # member is already in channel so return as successful
+                    return {}
+            is_public = channel['public_status']
+
+            if global_status == True or is_public == True:
+                user_dict = {'user_id':auth_user_id, 'channel_owner_status':global_status,}
+                channel['members'].append(user_dict)
+            else:
+                raise AccessError
+            break
+    
     return {
     }
 
