0f2b6dc9e29ab6db043bcee34539710bfbbcbaa6
Sesi Adams <Sesi@192-168-1-123.tpgi.com.au>
2021-03-06 19:22:02 +1100

    Updated Implementation - fixed code regarding creation of handle

--- src/auth.py
+++ src/auth.py
@@ -4,7 +4,7 @@ from src.data import data
 
 def auth_login_v1(email, password):
 
-    if re.fullmatch('^[a-zA-Z0-9]+[\\._]?[a-zA-Z0-9]+[@]\\w+[.]\\w{2,3}$', email) is None: 
+    if re.match('^[a-zA-Z0-9]+[\\._]?[a-zA-Z0-9]+[@]\\w+[.]\\w{2,3}$', email) is None: 
         raise InputError('Please enter a valid email address.') 
     
     for lists in data['users']:
@@ -25,57 +25,69 @@ def auth_register_v1(email, password, name_first, name_last):
     name_join = '' 
     handle = ''  
 
-    if re.fullmatch ('^[a-zA-Z0-9]+[\\._]?[a-zA-Z0-9]+[@]\\w+[.]\\w{2,3}$', email) is None: 
+    if re.match ('^[a-zA-Z0-9]+[\\._]?[a-zA-Z0-9]+[@]\\w+[.]\\w{2,3}$', email) is None: 
         return InputError('Please enter a valid email address.')
+ 
+    for lists in data('users'):
+        for user in lists:
+            if user['email_address'] == email:
+                raise InputError('Email already registered.') 
+
+    if password_length < 6:
+            raise InputError('Password is less than 6 characters.')    
+
+    if first_name_length < 1 or first_name_length > 50:
+        raise InputError('First name is not a valid length.')
+
+    if last_name_length < 1 or last_name_length > 50:
+        raise InputError('Last name is not a valid length.') 
+                
+    name_join = name_first + name_last
+
+    if len(name_join) > 20:
+        handle = name_join[0:20] 
     else:
-        for lists in data('users'):
-            for user in lists:
-                if user['email_address'] == email:
-                    raise InputError('Email already registered.') 
-                       
-        if password_length < 6:
-            raise InputError('Password is less than 6 characters.') 
-        else:
-            if first_name_length < 1 or first_name_length > 50:
-                raise InputError('First name is not a valid length.')
-            else:
-                if last_name_length < 1 or last_name_length > 50:
-                    raise InputError('Last name is not a valid length.') 
+        handle = name_join
+    
+    for character in handle:
+        if character == '@' or character.isspace(): 
+            raise InputError("No @ or whitespace allowed in handles.")
+
+    """   number = 0
+    numbering_start = True 
+    for user in data['users']:
+        if user['account_handle'] == handle:
+            handle = handle + str()
+            for user_handle in data['users']:
+                if numbering_start is True:
+                    handle = handle + str(number)
+                    numbering_start = False 
                 else:
-                    name_join = name_first + name_last
+                    number += 1
+                    handle = handle + str(number)
+    """
+    user_list = data['users']
+    i = 0
+    number = 0
+    updated_handle = handle 
+    while i < len(user_list):
+        user = user_list[i] 
+        if user['account_handle'] == updated_handle:
+            updated_handle = handle + str (number)
+            i = 0
+            number += 1
+        i += 1
 
-                    if len(name_join) > 20:
-                        for character in name_join[0:20]:
-                            handle += character 
-                    else:
-                        handle = name_join
-                    
-                    for characters in handle:
-                        if characters == '@' or characters == ' ' :
-                            raise InputError("No @ or whitespace allowed in handles.")
+    new_user = {
+        'first_name': name_first,
+        'last_name': name_last,
+        'email_address': email,
+        'account_password': password,
+        'global_owner_status': False,
+        'account_handle': updated_handle,
+        'user_id': len(data['users']) + 1,  
+    } 
 
-                    number = 0
-                    numbering_start = True 
-                    for lists in data['users']:
-                        for user in lists:
-                            if user['account_handle'] == handle:
-                                if numbering_start is True:
-                                    handle = handle + str(number)
-                                    numbering_start = False 
-                                else:
-                                    number += 1
-                                    handle = handle + str(number)
+    user_list.append(new_user)
 
-                    new_user = {
-                        'first_name': name_first,
-                        'last_name': name_last,
-                        'email_address': email,
-                        'account_password': password,
-                        'global_owner_status': False,
-                        'account_handle': handle,
-                        'user_id': len(data['users']) + 1,  
-                    } 
-
-                    data['users'].append(new_user)
-
-                    return new_user('user_id')
+    return new_user('user_id')
