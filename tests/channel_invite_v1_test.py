45b86d0e40b29527a14b707b3b7f8ed1cb3f16d8
Isabelle Ryan <z5205211@ad.unsw.edu.au>
2021-03-07 09:01:56 +1100

    changed channel invite tests to use fixtures correctly

--- tests/channel_invite_v1_test.py
+++ tests/channel_invite_v1_test.py
@@ -24,19 +24,20 @@ def create_user2():
     return auth_register_v1(email,password,firstname, lastname)['auth_user_id']
 
 @pytest.fixture
-def create_channel(is_public):
+def create_public_channel():
     name = "Testchannel"
     user_id = auth_register_v1("channelcreator@gmail.com", "TestTest", "channelcreator", "last")
-    return channels_create_v1(user_id, name, is_public)['channel_id']
+    return channels_create_v1(user_id, name, True)['channel_id']
 
 #input errors:
 #channel id is not valid
+clear_v1()
 def test_invalid_channel_id():
+    user_id = auth_register_v1("channelcreator2@gmail.com", "TestTest2", "channelcreator2", "last")['auth_user_id']
+    channel_id = channels_create_v1(user_id, 'Testchannel', True)
     clear_v1()
-    channel_id = create_channel(True)
-    clear_v1()
-    user_id_1 = create_user1()
-    user_id_2 = create_user2()
+    user_id_1 = auth_register_v1("user1@gmail.com", "TestTest2", "user1", "last")['auth_user_id']
+    user_id_2 = auth_register_v1("user2@gmail.com", "TestTest2", "user2", "last")['auth_user_id']
     
     with pytest.raises(InputError):
         channel_invite_v1(user_id_1, channel_id, user_id_2)
@@ -44,28 +45,24 @@ def test_invalid_channel_id():
 # u id is not valid
 def test_invalid_user_id():
     clear_v1()
-    user_id_1 = create_user1()
+    user_id_1 = auth_register_v1("user1@gmail.com", "TestTest2", "user1", "last")['auth_user_id']
     clear_v1()
-    channel_id = create_channel(True)
-    auth_user_id = auth_login_v1("channelcreator@gmail.com", "TestTest")['auth_user_id']
+    auth_user_id = auth_register_v1("channelcreator2@gmail.com", "TestTest2", "channelcreator2", "last")['auth_user_id']
+    channel_id = channels_create_v1(user_id, 'Testchannel', True)
     
     with pytest.raises(InputError):
         channel_invite(auth_user_id, channel_id, user_id_1)
+
 #access errors
 #auth user not in channel
-def test_unauthorised_user():
-    clear_v1()
-    user_id_1 = create_user1()
-    user_id_2 = create_user2()
-    channel_id = create_channel(True)
-
+clear_v1()
+def test_unauthorised_user(create_user_1, create_user_2, create_public_channel):
+    
     with pytest.raises(AccessError):
-        channel_invite_v1(user_id_2, channel_id, user_id_1)
+        channel_invite_v1(create_user_2, create_public_channel, create_user_1)
 
-def test_all_valid():
-    clear_v1()
-    user_id_1 = create_user1()
-    channel_id = create_channel(True)
+clear_v1()
+def test_all_valid(create_user_1, create_public_channel):
     auth_user_id = auth_login_v1("channelcreator@gmail.com", "TestTest")['auth_user_id']
 
     assert channel_invite_v1(auth_user_id, channel_id, user_id_1) == {}
