0260b5abdde3324135c0de5c97b634030984e799
Sesi Adams <Sesi@192-168-1-123.tpgi.com.au>
2021-03-07 11:22:38 +1100

    Updated to have correct indentation

--- src/auth.py
+++ src/auth.py
@@ -1,59 +1,61 @@
-import re 
+import re
 from src.error import InputError
-from src.data import data  
+from src.data import data
+
 
 def auth_login_v1(email, password):
 
-    if re.match('^[a-zA-Z0-9]+[\\._]?[a-zA-Z0-9]+[@]\\w+[.]\\w{2,3}$', email) == None: 
-        raise InputError('Please enter a valid email address.') 
-    
+    if re.match('^[a-zA-Z0-9]+[\\._]?[a-zA-Z0-9]+[@]\\w+[.]\\w{2,3}$', email) == None:
+        raise InputError('Please enter a valid email address.')
+
     for user in data['users']:
         if user['email_address'] == email:
             if user['account_password'] == password:
-                return {'auth_user_id':user['user_id']}    
+                return {'auth_user_id': user['user_id']}
             else:
-                raise InputError('Incorrect Password.') 
-    raise InputError('Email not found.') 
+                raise InputError('Incorrect Password.')
+    raise InputError('Email not found.')
+
 
 def auth_register_v1(email, password, name_first, name_last):
-    global data 
+    global data
     password_length = len(password)
     first_name_length = len(name_first)
     last_name_length = len(name_last)
 
-    if re.match('^[a-zA-Z0-9]+[\\._]?[a-zA-Z0-9]+[@]\\w+[.]\\w{2,3}$', email) is None: 
+    if re.match('^[a-zA-Z0-9]+[\\._]?[a-zA-Z0-9]+[@]\\w+[.]\\w{2,3}$', email) is None:
         raise InputError('Please enter a valid email address.')
- 
-    for user in data['users']: 
-            if user['email_address'] == email:
-                raise InputError('Email already registered.') 
+
+    for user in data['users']:
+        if user['email_address'] == email:
+            raise InputError('Email already registered.')
 
     if password_length < 6:
-            raise InputError('Password is less than 6 characters.')    
+        raise InputError('Password is less than 6 characters.')
 
     if first_name_length < 1 or first_name_length > 50:
         raise InputError('First name is not a valid length.')
 
     if last_name_length < 1 or last_name_length > 50:
-        raise InputError('Last name is not a valid length.') 
-                
+        raise InputError('Last name is not a valid length.')
+
     handle = name_first + name_last
 
     if len(handle) > 20:
-        handle = handle[0:20] 
-    
+        handle = handle[0:20]
+
     for character in handle:
-        if character == '@' or character.isspace() : 
+        if character == '@' or character.isspace():
             raise InputError("No @ or whitespace allowed in handles.")
 
     user_list = data['users']
     i = 0
     number = 0
-    updated_handle = handle 
+    updated_handle = handle
     while i < len(user_list):
-        user = user_list[i] 
+        user = user_list[i]
         if user['account_handle'] == updated_handle:
-            updated_handle = handle + str (number)
+            updated_handle = handle + str(number)
             i = 0
             number += 1
         i += 1
@@ -65,11 +67,11 @@ def auth_register_v1(email, password, name_first, name_last):
         'last_name': name_last,
         'email_address': email,
         'account_password': password,
-        'permission_id': permission_id, 
+        'permission_id': permission_id,
         'account_handle': updated_handle,
-        'user_id': len(data['users']) + 1,  
-    } 
+        'user_id': len(data['users']) + 1,
+    }
 
     user_list.append(new_user)
 
-    return {'auth_user_id':new_user['user_id']}
+    return {'auth_user_id': new_user['user_id']}
