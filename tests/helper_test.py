07a17a216b5795900c529dbfbdf74683a01c3bda
Isabelle Ryan <z5205211@ad.unsw.edu.au>
2021-03-07 11:55:22 +1100

    added check valid channel_id, but requires channels_create_v1 implementation before testing

--- tests/helper_test.py
+++ tests/helper_test.py
@@ -1,7 +1,7 @@
-from src.helper import check_auth_user_id_v1
+from src.helper import check_auth_user_id_v1, check_channel_id_v1
 from src.auth import auth_register_v1
 from src.other import clear_v1
-from src.error import AccessError
+from src.error import AccessError, InputError
 import pytest
 
 
@@ -10,8 +10,9 @@ def test_invalid_user_id():
     with pytest.raises(AccessError):
         check_auth_user_id_v1(1)
 
-
 def test_valid_user_id():
     clear_v1()
     user_id = auth_register_v1("test@gmail.com", "password", "first", "last")['auth_user_id']
     assert check_auth_user_id_v1(user_id) == None
+
+
