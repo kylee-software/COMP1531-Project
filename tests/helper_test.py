8d393edde34329a4a89511b88816a368c3f4e45d
Isabelle Ryan <z5205211@ad.unsw.edu.au>
2021-03-17 11:20:25 +1100

    fixed minor error - wrote the return values the wrong way in the tests

--- tests/helper_test.py
+++ tests/helper_test.py
@@ -8,21 +8,21 @@ from src.channels import channels_create_v1
 
 def test_invalid_user_id():
     clear_v1()
-    assert check_auth_user_id_v1(1) == True
+    assert check_auth_user_id_v1(1) == False
 
 def test_valid_user_id():
     clear_v1()
     user_id = auth_register_v1("test@gmail.com", "password", "first", "last")['auth_user_id']
-    assert check_auth_user_id_v1(user_id) == False
+    assert check_auth_user_id_v1(user_id) == True
 
 def test_invalid_channel_id():
     clear_v1()
-    assert check_channel_id_v1(1) ==True
+    assert check_channel_id_v1(1) == False
 
 def test_valid_channel_id():
     clear_v1()
     user_id = auth_register_v1("test@gmail.com", "password", "first", "last")['auth_user_id']
     channel_id = channels_create_v1(user_id, "Channelname", True)['channel_id']
-    assert check_channel_id_v1(channel_id) == False
+    assert check_channel_id_v1(channel_id) == True
 
 
