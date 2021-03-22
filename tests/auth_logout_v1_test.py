f279ee6ff9569012bfb79b0aa2bb844c63642b3d
Michael Kappel <z1567704@unsw.edu.au>
2021-03-22 13:01:00 +1100

    added some python tests for auth_logout

---
+++ tests/auth_logout_v1_test.py
@@ -0,0 +1,16 @@
+from jwt import algorithms
+import pytest
+import jwt
+from src.auth import auth_logout, auth_register_v1
+
+def test_invalid_token():
+    invalid_token = jwt.encode({'some' : 'value'}, "TestingSecret", algorithm='HS256')
+    assert auth_logout(invalid_token) == False
+
+def test_valid_token():
+    email = "test2email@gmail.com"
+    password = "TestTest2"
+    firstname = "firstname2"
+    lastname = "lastname2"
+    token = auth_register_v1(email,password,firstname, lastname)['auth_user_id']
+    assert auth_logout(token) == True
