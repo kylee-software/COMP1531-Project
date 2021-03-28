97e812b0d60cc23cb9b16407d08b27dda6c7e29b
Michael Kappel <michael.kappel@student.unsw.edu.au>
2021-03-29 05:14:24 +1100

    initializes messages and notification if fields don't yet exist in dictionary

--- src/message.py
+++ src/message.py
@@ -25,7 +25,10 @@ def message_send_v1(token, channel_id, message):
             for user in channel['users']:
                 if user['user_id'] == user_id:
                     found_user = True
-                    channel['messages'].insert(0, new_message)
+                    if channel.get('messages') == None:
+                        channel['messages'] = [new_message]
+                    else:
+                        channel['messages'].insert(0, new_message)
                 
     if found_user:
         for channel in data['channels']:
@@ -35,7 +38,10 @@ def message_send_v1(token, channel_id, message):
                         if user['user_id'] == user1['user_id']:
                             if tagged_handles.count(user1['handle']) != 0:
                                 notification_message = f"{user_handle} tagged you in {channel['name']}: {message[0:20]}"
-                                user['notifications'].insert(0, {channel_id, -1, notification_message})
+                                if user.get['notifications'] == None:
+                                    user['notificatiosn'] = [{channel_id, -1, notification_message}]
+                                else:
+                                    user['notifications'].insert(0, {channel_id, -1, notification_message})
     else:
         raise AccessError('You have not joined this channel')
 
