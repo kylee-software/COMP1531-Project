b8aafef6a17f7486f83195b42e1447abd98268b4
Sesi Adams <z5074999@ad.unsw.edu.au>
2021-03-31 14:31:30 +1100

    Http tests for auth_register_v2

---
+++ http_tests/auth_register_http_test.py
@@ -0,0 +1,71 @@
+import requests
+from src import config
+from src.helper import is_valid_token
+
+
+def test_given_email_is_invalid():
+    requests.delete(f"{config} /clear/v1")
+    registration = requests.post(f"{config}/register/v2",
+                                 json={'email': 'test.unsw.edu.au', 'password': 'password', 'name_first': 'test123', 'name_last': 'last123'})
+    assert registration.status_code == 400
+
+
+def test_email_already_exists():
+    requests.delete(f"{config} /clear/v1")
+    requests.post(f"{config}/register/v2",
+                  json={'email': 'test@unsw.edu.au', 'password': 'password', 'name_first': 'test123', 'name_last': 'last123'})
+    registration = requests.post(f"{config}/register/v2",
+                                 json={'email': 'test@unsw.edu.au', 'password': 'password2', 'name_first': 'test321', 'name_last': 'last321'})
+    assert registration.status_code == 400
+
+
+def test_password_incorrect_length():
+    requests.delete(f"{config} /clear/v1")
+    registration = requests.post(f"{config}/register/v2",
+                                 json={'email': 'test@unsw.edu.au', 'password': ' ', 'name_first': 'test123', 'name_last': 'last123'})
+    assert registration.status_code == 400
+
+
+def test_first_name_valid_length():
+    requests.delete(f"{config} /clear/v1")
+    registration = requests.post(f"{config}/register/v2",
+                                 json={'email': 'test@unsw.au', 'password': 'password', 'name_first': 'thisfirstnameistoolongandcontainsspecialcharacters##^^&&**!!123123123', 'name_last': 'last123'})
+    assert registration.status_code == 400
+
+
+def test_last_name_valid_length():
+    requests.delete(f"{config} /clear/v1")
+    registration = requests.post(f'{config}/register/v2',
+                                 json={'email': 'test@unsw.au', 'password': 'password', 'name_first': 'firstname', 'name_last': 'thislastnamecontainsspecialcharacters##^^&&**!!123123123'})
+    assert registration.status_code == 400
+
+
+def test_no_whitespace():
+    requests.delete(f"{config} /clear/v1")
+    registration = requests.post(f'{config}/register/v2',
+                                 json={'email': 'test@unsw.au', 'password': 'password', 'name_first': 'first name', 'name_last': 'last name'})
+    assert registration.status_code == 400
+
+
+def test_no_at_symbol():
+    requests.delete(f"{config} /clear/v1")
+    registration = requests.post(f'{config}/register/v2',
+                                 json={'email': 'test@unsw.au', 'password': 'password', 'name_first': 'firstn@me', 'name_last': 'l@stn@me'})
+    assert registration.status_code == 400
+
+
+def test_no_at_and_whitespace():
+    requests.delete(f"{config} /clear/v1")
+    registration = requests.post(
+        f'{config}/register/v2',
+        json={'email': 'test@unsw.au', 'password': 'password', 'name_first': '@ first ', 'name_last': '@ last '})
+    assert registration.status_code == 400
+
+
+def test_registration_successful():
+    requests.delete(f"{config} /clear/v1")
+    registration = requests.post(f'{config}/register/v2', json={'email': 'test@unsw.au',
+                                                                'password': 'password', 'name_first': 'firstname', 'name_last': 'lastname'})
+    registration_details = registration.json()
+    assert registration.status_code == 200
+    assert is_valid_token(registration_details['token'])
