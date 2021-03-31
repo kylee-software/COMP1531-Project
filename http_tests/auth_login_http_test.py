ccf293faf5b74c9e8c04f683d984d5d02f5b6609
Sesi Adams <z5074999@ad.unsw.edu.au>
2021-03-31 23:51:38 +1100

    Test urls updated to follow  echo http test format

--- http_tests/auth_login_http_test.py
+++ http_tests/auth_login_http_test.py
@@ -6,8 +6,8 @@ from src.helper import is_valid_token
 
 
 def test_invalid_email():
-    requests.delete(f"{config} /clear/v1")
-    login_call = requests.post(f"{config}/auth/login/v2",
+    requests.delete(config.url + '/clear/v1')
+    login_call = requests.post(config.url + '/auth/login/v2',
                                json={'email': 'testing.unsw.au', 'password': 'password'})
     assert login_call.status_code == 400
 
@@ -15,8 +15,8 @@ def test_invalid_email():
 
 
 def test_email_nonexistent():
-    requests.delete(f"{config} /clear/v1")
-    login_call = requests.post(f"{config}/auth/login/v2",
+    requests.delete(config.url + '/clear/v1')
+    login_call = requests.post(config.url + '/auth/login/v2',
                                json={'email': 'testing123@unsw.au', 'password': 'password'})
     assert login_call.status_code == 400
 
@@ -24,10 +24,10 @@ def test_email_nonexistent():
 
 
 def test_password_incorrect():
-    requests.delete(f"{config} /clear/v1")
-    requests.post(f"{config}/auth/register/v2",
+    requests.delete(config.url + '/clear/v1')
+    requests.post(config.url + '/auth/register/v2',
                   json={'email': 'testing123@unsw.au', 'password': 'password', 'name_first': 'testing123', 'name_last': 'last123'})
-    login_call = requests.post(f"{config}/auth/login/v2",
+    login_call = requests.post(config.url + '/auth/login/v2',
                                json={'email': 'testing123@unsw.au', 'password': 'failed123'})
     assert login_call.status_code == 400
 
@@ -35,10 +35,10 @@ def test_password_incorrect():
 
 
 def test_correct_login_details():
-    requests.delete(f"{config} /clear/v1")
-    registration = requests.post(f"{config}/auth/register/v2",
+    requests.delete(config.url + '/clear/v1')
+    registration = requests.post(config.url + '/auth/register/v2',
                                  json={'email': 'testing123@unsw.au', 'password': 'password', 'name_first': 'testing123', 'name_last': 'last123'})
-    login_call = requests.post(f"{config}/auth/login/v2",
+    login_call = requests.post(config.url + '/auth/login/v2',
                                json={'email': 'testing123@unsw.au', 'password': 'password'})
     login_details = login_call.json()
     registration_details = registration.json()
