ca5be487ff4fd8aa1aed4238d1e89772acfb1cbe
Sesi Adams <z5074999@ad.unsw.edu.au>
2021-03-31 19:07:25 +1100

    Updated test for successful registration

--- tests/auth_register_v1_test.py
+++ tests/auth_register_v1_test.py
@@ -1,146 +1,170 @@
 import pytest
 from src.auth import auth_login_v1, auth_register_v1
-from src.other import clear_v1 
-from src.error import InputError 
+from src.other import clear_v1
+from src.error import InputError
+from src.helper import is_valid_token, is_valid_user_id
 
 # Test exception - Email entered is not valid (wrong format)
+
+
 def test_given_email_is_invalid():
-    clear_v1() 
+    clear_v1()
 
     with pytest.raises(InputError):
         auth_register_v1('@unsw.edu.au', 'password', 'first1', 'last1')
-    
+
     with pytest.raises(InputError):
         auth_register_v1('test.unsw.edu.au', 'password', 'first2', 'last2')
 
     with pytest.raises(InputError):
         auth_register_v1('test@.au', 'password', 'first3', 'last3')
-    
+
     with pytest.raises(InputError):
-        auth_register_v1('test_special!!!@unsw.edu.au', 'password', 'firstspecial', 'lastspecial')
-    
-    clear_v1() 
+        auth_register_v1('test_special!!!@unsw.edu.au',
+                         'password', 'firstspecial', 'lastspecial')
+
+    clear_v1()
 
 # Test exception - Email entered already exists (belongs to an existing user)
+
+
 def test_email_already_exists():
     clear_v1()
 
     auth_register_v1('testing123@unsw.au', 'password', 'first123', 'last123')
     with pytest.raises(InputError):
         auth_register_v1('testing123@unsw.au', 'hello123', 'first1', 'last1')
-    
+
     auth_register_v1('testing567@unsw.au', 'password', 'first567', 'last567')
     with pytest.raises(InputError):
         auth_register_v1('testing567@unsw.au', 'hello567', 'first2', 'last2')
-    
+
     auth_register_v1('testing890@unsw.au', 'password', 'first890', 'last890')
     with pytest.raises(InputError):
         auth_register_v1('testing890@unsw.au', 'hello890', 'first3', 'last3')
-    
-    clear_v1() 
+
+    clear_v1()
 
 # Test exception - Password given is less than 6 Characters
+
+
 def test_password_incorrect_length():
     clear_v1()
 
     with pytest.raises(InputError):
-        auth_register_v1('testing123@unsw.au', '', 'first1', 'last1')  
+        auth_register_v1('testing123@unsw.au', '', 'first1', 'last1')
 
     with pytest.raises(InputError):
-        auth_register_v1('testing567@unsw.au', '2', 'first2', 'last2') 
-    
-    with pytest.raises(InputError):
-        auth_register_v1('testing890@unsw.au', '@3456', 'first3', 'last3') 
-    
-    clear_v1() 
+        auth_register_v1('testing567@unsw.au', '2', 'first2', 'last2')
 
-# Test exception - First name given is between 1 and 50 inclusive 
+    with pytest.raises(InputError):
+        auth_register_v1('testing890@unsw.au', '@3456', 'first3', 'last3')
+
+    clear_v1()
+
+# Test exception - First name given is between 1 and 50 inclusive
+
+
 def test_first_name_valid_length():
-    clear_v1() 
+    clear_v1()
 
     with pytest.raises(InputError):
-        auth_register_v1('testing123@unsw.au', 'password', '', 'last1') 
-    
-    with pytest.raises(InputError):
-        auth_register_v1('testing567@unsw.au', 'password', 'thisfirstnameismorethanfiftycharacterslong123123123123123', 'last2')
-    
-    with pytest.raises(InputError):
-        auth_register_v1('testing890@unsw.au', 'password', 'thisfirstnamecontainsspecialcharacters##^^&&**!!123123123', 'last3')  
+        auth_register_v1('testing123@unsw.au', 'password', '', 'last1')
 
-    clear_v1()  
+    with pytest.raises(InputError):
+        auth_register_v1('testing567@unsw.au', 'password',
+                         'thisfirstnameismorethanfiftycharacterslong123123123123123', 'last2')
 
-# Test exception - Last name given is between 1 and 50 inclusive 
+    with pytest.raises(InputError):
+        auth_register_v1('testing890@unsw.au', 'password',
+                         'thisfirstnamecontainsspecialcharacters##^^&&**!!123123123', 'last3')
+
+    clear_v1()
+
+# Test exception - Last name given is between 1 and 50 inclusive
+
+
 def test_last_name_valid_length():
     clear_v1()
-    
+
     with pytest.raises(InputError):
         auth_register_v1('testing123@unsw.au', 'password', 'first1', '')
-    
-    with pytest.raises(InputError):
-        auth_register_v1('testing567@unsw.au', 'password', 'first2', 'thislastnameismorethanfiftycharacterslong123123123123123') 
-    
-    with pytest.raises(InputError):
-        auth_register_v1('testing890@unsw.au', 'password', 'first3', 'thislastnamecontainsspecialcharacters##^^&&**!!123123123')
-    
-    clear_v1() 
 
-# Test - if handle generated has whitespace it raises an error as not allowed 
+    with pytest.raises(InputError):
+        auth_register_v1('testing567@unsw.au', 'password', 'first2',
+                         'thislastnameismorethanfiftycharacterslong123123123123123')
+
+    with pytest.raises(InputError):
+        auth_register_v1('testing890@unsw.au', 'password', 'first3',
+                         'thislastnamecontainsspecialcharacters##^^&&**!!123123123')
+
+    clear_v1()
+
+# Test - if handle generated has whitespace it raises an error as not allowed
+
+
 def test_no_whitespace():
-    clear_v1() 
+    clear_v1()
 
     with pytest.raises(InputError):
-        auth_register_v1('testing123@unsw.au', 'password', 'first name', 'last name')
-    
+        auth_register_v1('testing123@unsw.au', 'password',
+                         'first name', 'last name')
+
     with pytest.raises(InputError):
-        auth_register_v1('testing567@unsw.au', 'password', '   first', 'last') 
+        auth_register_v1('testing567@unsw.au', 'password', '   first', 'last')
 
     with pytest.raises(InputError):
         auth_register_v1('testing890@unsw.au', 'password', 'first', 'last   ')
-    
-    clear_v1() 
+
+    clear_v1()
 
 # Test - if handle generated has @ it raises an error as not allowed
+
+
 def test_no_at_symbol():
     clear_v1()
 
     with pytest.raises(InputError):
-        auth_register_v1('testing123@unsw.au', 'password', 'firstn@me', 'l@stn@me') 
-    
+        auth_register_v1('testing123@unsw.au', 'password',
+                         'firstn@me', 'l@stn@me')
+
     with pytest.raises(InputError):
         auth_register_v1('testing567@unsw.au', 'password', '@first', 'last')
 
     with pytest.raises(InputError):
         auth_register_v1('testing890@unsw.au', 'password', 'first', 'last@')
-    
-    clear_v1() 
 
-# Test - if handle generated contains both @ and whitespace it raises an error as not allowed 
+    clear_v1()
+
+# Test - if handle generated contains both @ and whitespace it raises an error as not allowed
+
+
 def test_no_at_and_whitespace():
-    clear_v1() 
+    clear_v1()
 
     with pytest.raises(InputError):
-        auth_register_v1('testing123@unsw.au', 'password', '@ first ', '@ last ')
-    
-    with pytest.raises(InputError):
-        auth_register_v1('testing567@unsw.au', 'password', 'first @ name', 'last @ name')
+        auth_register_v1('testing123@unsw.au', 'password',
+                         '@ first ', '@ last ')
 
     with pytest.raises(InputError):
-        auth_register_v1('testing890@unsw.au', 'password', 'first @@@' , 'last @@@ ') 
-    
-    clear_v1() 
+        auth_register_v1('testing567@unsw.au', 'password',
+                         'first @ name', 'last @ name')
 
-# Test registration of details was successful - if registration is successful then you can login 
+    with pytest.raises(InputError):
+        auth_register_v1('testing890@unsw.au', 'password',
+                         'first @@@', 'last @@@ ')
+
+    clear_v1()
+
+# Test registration of details was successful - if registration is successful then you can login
+
+
 def test_registration_successful():
     clear_v1()
 
-    userid_1 = auth_register_v1('testing123@unsw.au', 'password', 'first1', 'last1') 
-    userid_2 = auth_register_v1('testing567@unsw.au', 'password', 'first2', 'last2') 
-    userid_3 = auth_register_v1('testing890@unsw.au', 'password', 'first3', 'last3') 
+    userid_1 = auth_register_v1(
+        'testing123@unsw.au', 'password', 'first1', 'last1')
+    assert is_valid_token(userid_1['token'])
+    assert is_valid_user_id(userid_1['auth_user_id'])
 
-    assert auth_login_v1('testing123@unsw.au', 'password') == userid_1
-    assert auth_login_v1('testing567@unsw.au', 'password') == userid_2
-    assert auth_login_v1('testing890@unsw.au', 'password') == userid_3
-
-    clear_v1() 
-    
-
+    clear_v1()
