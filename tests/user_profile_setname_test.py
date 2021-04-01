2eb755fa969a7f16026dc30b2ce8881b8e26dbad
Sesi Adams <z5074999@ad.unsw.edu.au>
2021-04-02 02:36:56 +1100

    Return user details in variable - success case

--- tests/user_profile_setname_test.py
+++ tests/user_profile_setname_test.py
@@ -3,6 +3,7 @@ from src.auth import auth_register_v1
 from src.user import user_profile_setname_v1
 from src.error import InputError
 from src.other import clear_v1
+from src.helper import find_user, load_data
 
 
 def test_auth_user_id_valid():
@@ -44,7 +45,10 @@ def test_successful_setname():
     user_1 = auth_register_v1('test@unsw.au', 'password', 'first', 'last')
     user_profile_setname_v1(user_1['auth_user_id'], 'firstname', 'lastname')
 
-    assert user_1['first_name'] == 'firstname'
-    assert user_1['last_name'] == 'lastname'
+    data = load_data()
+    user_1_details = find_user(user_1['auth_user_id'], data)
+
+    assert user_1_details['first_name'] == 'firstname'
+    assert user_1_details['last_name'] == 'lastname'
 
     clear_v1()
