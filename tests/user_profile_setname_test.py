e77cc5a33bf5725d8aeb672a26a2098c2aae655a
Sesi Adams <z5074999@ad.unsw.edu.au>
2021-04-02 02:25:51 +1100

    Updated to test for valid auth_user_id

--- tests/user_profile_setname_test.py
+++ tests/user_profile_setname_test.py
@@ -5,6 +5,17 @@ from src.error import InputError
 from src.other import clear_v1
 
 
+def test_auth_user_id_valid():
+    clear_v1()
+
+    user_1 = auth_register_v1('test@unsw.au', 'password', 'first', 'last')
+
+    with pytest.raises(InputError):
+        user_profile_setname_v1(100, 'firstname', 'lastname')
+
+    clear_v1()
+
+
 def test_first_name_incorrect_length():
     clear_v1()
 
