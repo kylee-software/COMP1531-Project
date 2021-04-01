8219b8bb2f5a4d8cfefe38f1b0ad19485bb626ef
Sesi Adams <z5074999@ad.unsw.edu.au>
2021-04-01 21:01:47 +1100

    Updated success case test

--- tests/channel_addowner_v1_test.py
+++ tests/channel_addowner_v1_test.py
@@ -4,6 +4,7 @@ from src.channel import channel_addowner_v1
 from src.channels import channels_create_v1
 from src.other import clear_v1
 from src.error import InputError, AccessError
+from src.helper import is_user_in_channel, load_data, find_user_channel_owner_status
 
 
 def test_invalid_channel():
@@ -62,8 +63,12 @@ def test_successful_addowner():
     admin = auth_register_v1('test@unsw.au', 'password1', 'first1', 'last1')
     member = auth_register_v1('test1@unsw.au', 'password2', 'first2', 'last2')
     channel_id = channels_create_v1(admin['token'], 'channel_1', True)
-    with pytest.raises(InputError):
-        channel_addowner_v1(
-            admin['auth_user_id'], channel_id['channel_id'], member['auth_user_id'])
+    channel_addowner_v1(
+        admin['auth_user_id'], channel_id['channel_id'], member['auth_user_id'])
+
+    data = load_data()
+    assert is_user_in_channel(channel_id['channel_id'], data)
+    assert find_user_channel_owner_status(
+        channel_id['channel_id'], member['auth_user_id'], data) == 1
 
     clear_v1()
