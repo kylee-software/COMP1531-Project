efe0ab9011b09d4cf899b8f9a0a87a3b9473c867
Sesi Adams <z5074999@ad.unsw.edu.au>
2021-04-02 01:51:14 +1100

    Pytests for user/profile/setname/v2

---
+++ tests/user_profile_setname_test.py
@@ -0,0 +1,41 @@
+import pytest
+from src.auth import auth_register_v1
+from src.user import user_profile_setname_v1
+from src.error import InputError
+from src.other import clear_v1
+
+
+def test_first_name_incorrect_length():
+    clear_v1()
+
+    user_1 = auth_register_v1('test@unsw.au', 'password', 'first', 'last')
+
+    with pytest.raises(InputError):
+        user_profile_setname_v1(
+            user_1['auth_user_id'], 'thisfirstnamecontainsspecialcharacters##^^&&**!!123123123', 'lastname')
+
+    clear_v1()
+
+
+def test_last_name_incorrect_length():
+    clear_v1()
+
+    user_1 = auth_register_v1('test@unsw.au', 'password', 'first', 'last')
+
+    with pytest.raises(InputError):
+        user_profile_setname_v1(user['auth_user_id'], 'firstname',
+                                'thislastnamecontainsspecialcharacters##^^&&**!!123123123')
+
+    clear_v1()
+
+
+def test_successful_setname():
+    clear_v1()
+
+    user_1 = auth_register_v1('test@unsw.au', 'password', 'first', 'last')
+    user_profile_setname_v1(user_1['auth_user_id'], 'firstname', 'lastname')
+
+    assert user_1['first_name'] == 'firstname'
+    assert user_1['last_name'] == 'lastname'
+
+    clear_v1()
