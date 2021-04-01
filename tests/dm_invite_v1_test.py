7cafc42d9774d3563b8d875b029070a4a16b2143
Michael Kappel <z1567704@unsw.edu.au>
2021-04-01 20:53:50 +1100

    added tests for dm_invite function

---
+++ tests/dm_invite_v1_test.py
@@ -0,0 +1,59 @@
+import pytest
+from src.auth import auth_register_v1
+from src.dm import dm_create, dm_invite, dm_details
+from src.helper import is_valid_token
+from src.error import InputError, AccessError
+from src.other import clear_v1
+import jwt
+
+@pytest.fixture
+def token():
+    email = "test10email@gmail.com"
+    password = "TestTest10"
+    firstname = "firstname10"
+    lastname = "lastname10"
+    return auth_register_v1(email,password,firstname, lastname)['token']
+
+@pytest.fixture
+def user_id_list():
+    u_ids = []
+    for i in range(5):
+        email = f"test{i}email@gmail.com"
+        password = f"TestTest{i}"
+        firstname = f"firstname{i}"
+        lastname = f"lastname{i}"
+        u_ids.append(auth_register_v1(email,password,firstname, lastname)['auth_user_id'])
+
+@pytest.fixture
+def clear():
+    clear_v1()
+
+
+def test_invalid_dm_id(clear, token, user_id_list):
+    with pytest.raises(InputError):
+        dm_invite(token, 'invalid_dm_id', user_id_list[0])
+
+def test_invalid_user_id(clear, token, user_id_list):
+    dm = dm_create(token, user_id_list)
+    with pytest.raises(InputError):
+        dm_invite(token, dm['dm_id'], 'invalid_user_id')
+
+def test_invalid_token(clear, token, user_id_list):
+    dm = dm_create(token, user_id_list[1:])
+    with pytest.raises(AccessError):
+        dm_invite(jwt.encode({'Test': 'Token'}, 'TestSecret', algorithm='HS256'), dm['dm_id'], user_id_list[0])
+
+def test_user_not_in_dm(clear, token, user_id_list):
+    email = "test91gmail@gmail.com"
+    password = "TestTest91"
+    firstname = "firstname91"
+    lastname = "lastname91"
+    token2 = auth_register_v1(email,password,firstname, lastname)['token']
+    dm = dm_create(token, user_id_list[1:])
+    with pytest.raises(AccessError):
+        dm_invite(token2, dm['dm_id'], user_id_list[0])
+
+def test_everything_valid(clear, token, user_id_list):
+    dm = dm_create(token, user_id_list[1:])
+    dm_invite(token, dm['dm_id'], user_id_list[0])
+    assert len(dm_details(token, dm['dm_id'])['members']) == 6
