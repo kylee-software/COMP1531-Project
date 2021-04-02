dfbd19e9f5d669c9aba22cc1ed20c9c2c61f1a62
Michael Kappel <z1567704@unsw.edu.au>
2021-04-02 23:48:44 +1100

    fixed message_send function to work with helper functions and valid returns

--- src/helper.py
+++ src/helper.py
@@ -5,27 +5,27 @@ import json
 SECRET = 'WED09B-ECHO'
 
 def return_valid_tagged_handles(message, channel_id):
+    data = load_data()
     split_message = message.split()
     handles = []
     for word in split_message:
         if word.startswith('@'):
             handles.append(word.strip('@'))
+
     real_handles = []
     for handle in handles:
-        for user in data['users']:
-            if user['handle'] == handle:
-                real_handles.append(handle)
+        if next((user for user in data['users'] if user['account_handle'] == handle), False):
+            real_handles.append(handle)
+
     real_handles_in_channel = []
-    for channel in data['channels']:
-        if channel['channel_id'] == channel_id:
-            for handle in real_handles:
-                for member in channel['members']:
-                    if member['handle'] == handle:
-                        real_handles_in_channel.append(handle)
+    channel = next((channel for channel in data['channels'] if channel['channel_id'] == channel_id), False)
+    for handle in real_handles:
+        for member in channel['members']:
+            m_handle = next(user['account_handle'] for user in data['users'] if user['user_id'] == member['user_id'])
+            if m_handle == handle:
+                real_handles_in_channel.append(handle)
+
     return real_handles_in_channel
-
-def check_auth_user_id_v1(auth_user_id):
-
 
 def is_valid_user_id(auth_user_id):
     '''
--- src/message.py
+++ src/message.py
@@ -1,28 +1,28 @@
-from src.helper import valid_token, get_user_from_token, get_handle_from_token, return_valid_tagged_handles, load, save
+from src.helper import is_valid_token, return_valid_tagged_handles, load_data, save_data
 from src.error import AccessError, InputError
 from datetime import datetime
 
 
 def message_send_v1(token, channel_id, message):
-    data = load()
-    if not valid_token(token):
+    data = load_data()
+    if not is_valid_token(token):
         raise AccessError('Unauthorised User')
-    
+    token = is_valid_token(token)
     if len(message) > 1000:
         raise InputError('Message is longer than 1000 characters')
     
-    user_id = get_user_from_token(token)
+    
     channel = next(channel for channel in data['channels'] if channel['channel_id'] == channel_id)
-    msg_user = next((user for user in channel['members'] if user['user_id'] == user_id), False)
+    msg_user = next((user for user in channel['members'] if user['user_id'] == token['user_id']), False)
     if not msg_user:
         raise AccessError('You have not joined this channel')
     else:
 
-        new_message = {'message_id' : data['msg_id_counter'] + 1, 'message_author' : user_id,
+        new_message = {'message_id' : data['msg_counter'] + 1, 'message_author' : token['user_id'],
                             'message' : message, "time_created" : datetime.now()}
 
         tagged_handles = return_valid_tagged_handles(message, channel_id)
-        user_handle = get_handle_from_token(token)
+        user_handle = next(user['account_handle'] for user in data['users'] if user['user_id'] == token['user_id'])
 
         if channel.get('messages') == None:
             channel['messages'] = [new_message]
@@ -30,7 +30,7 @@ def message_send_v1(token, channel_id, message):
             channel['messages'].insert(0, new_message)     
 
         for user in channel['users']:
-            if tagged_handles.count(user['handle']) != 0:
+            if tagged_handles.count(user['account_handle']) != 0:
                notification_message = f"{user_handle} tagged you in {channel['name']}: {message[0:20]}"
                if user.get['notifications'] == None:
                    user['notificatiosn'] = [{channel_id, -1, notification_message}]
@@ -38,8 +38,8 @@ def message_send_v1(token, channel_id, message):
                    user['notifications'].insert(0, {channel_id, -1, notification_message})
 
         data['msg_id_counter'] += 1
-        save(data)
-        return data['msg_id_counter']
+        save_data(data)
+        return data['msg_counter']
 
     """found_user = False
     for channel in data['channels']:
