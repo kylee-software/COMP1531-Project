a587c8282eea651c7c4df53c2062249956d9c6d2
Sesi Adams <z5074999@ad.unsw.edu.au>
2021-04-02 18:24:39 +1100

    Updated auth_register to v2

--- tests/channel_addowner_v1_test.py
+++ tests/channel_addowner_v1_test.py
@@ -1,5 +1,5 @@
 import pytest
-from src.auth import auth_login_v1, auth_register_v1
+from src.auth import auth_register_v2
 from src.channel import channel_addowner_v1
 from src.channels import channels_create_v1
 from src.other import clear_v1
@@ -10,32 +10,29 @@ from src.helper import is_user_in_channel, load_data, find_user_channel_owner_st
 def test_invalid_channel():
     clear_v1()
 
-    admin = auth_register_v1('test@unsw.au', 'password1', 'first1', 'last1')
-    member = auth_register_v1('test1@unsw.au', 'password2', 'first2', 'last2')
+    admin = auth_register_v2('test@unsw.au', 'password1', 'first1', 'last1')
+    member = auth_register_v2('test1@unsw.au', 'password2', 'first2', 'last2')
     with pytest.raises(InputError):
         channel_addowner_v1(admin['auth_user_id'], 100, member['auth_user_id'])
 
     clear_v1()
 
 
-"""
 def test_user_nonexistent():
     clear_v1()
 
-    admin = auth_register_v1('test@unsw.au', 'password', 'first', 'last')
-    channel_id = channels_create_v1(admin['auth_user_id'], 'channel_1', True)
+    admin = auth_register_v2('test@unsw.au', 'password', 'first', 'last')
+    channel_id = channels_create_v1(admin['auth_user_id'], 'channel_1', True) 
     with pytest.raises(InputError):
         channel_addowner_v1(admin['auth_user_id'],
                             channel_id['channel_id'], 10)
 
     clear_v1()
 
-"""
-
 
 def test_user_already_owner():
     clear_v1()
-    admin = auth_register_v1('test@unsw.au', 'password', 'first', 'last')
+    admin = auth_register_v2('test@unsw.au', 'password', 'first', 'last')
     channel_id = channels_create_v1(admin['auth_user_id'], 'channel_1', True)
     with pytest.raises(InputError):
         channel_addowner_v1(admin['auth_user_id'],
@@ -47,10 +44,10 @@ def test_user_already_owner():
 def test_not_owner_of_channel_or_dreams():
     clear_v1()
 
-    admin_1 = auth_register_v1('test@unsw.au', 'password', 'first', 'last')
-    member_1 = auth_register_v1(
+    admin_1 = auth_register_v2('test@unsw.au', 'password', 'first', 'last')
+    member_1 = auth_register_v2(
         'test2@unsw.au', 'password2', 'first2', 'last2')
-    member_2 = auth_register_v1(
+    member_2 = auth_register_v2(
         'test3@unsw.au', 'password3', 'first3', 'last3')
     channel_id = channels_create_v1(admin_1['auth_user_id'], 'channel_1', True)
     with pytest.raises(AccessError):
@@ -63,8 +60,8 @@ def test_not_owner_of_channel_or_dreams():
 def test_successful_addowner():
     clear_v1()
 
-    admin = auth_register_v1('test@unsw.au', 'password1', 'first1', 'last1')
-    member = auth_register_v1('test1@unsw.au', 'password2', 'first2', 'last2')
+    admin = auth_register_v2('test@unsw.au', 'password1', 'first1', 'last1')
+    member = auth_register_v2('test1@unsw.au', 'password2', 'first2', 'last2')
     channel_id = channels_create_v1(admin['auth_user_id'], 'channel_1', True)
     channel_addowner_v1(
         admin['auth_user_id'], channel_id['channel_id'], member['auth_user_id'])
