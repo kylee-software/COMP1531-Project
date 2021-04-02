0085a052dca7a5f1c133e4502265ef46620e3358
Michael Kappel <z1567704@unsw.edu.au>
2021-04-02 14:05:35 +1100

    make function more pythonic cleaned up tests to use proper return values

--- tests/channels_list_v1_test.py
+++ tests/channels_list_v1_test.py
@@ -6,8 +6,8 @@ from src.error import AccessError
 from src.other import clear_v1
 
 @pytest.fixture
-def auth_user_id():
-    return auth_register_v1("test@unsw.com", 'testPassword8', 'Test', "User")['auth_user_id']
+def token():
+    return auth_register_v1("test@unsw.com", 'testPassword8', 'Test', "User")
 
 @pytest.fixture
 def names():
@@ -17,37 +17,37 @@ def names():
 def clear():
     clear_v1()
 
-def test_no_channels(clear, auth_user_id):
-    returnDict = channels_list_v1(auth_user_id)
+def test_no_channels(clear, token):
+    returnDict = channels_list_v1(token)
     assert returnDict['channels'] == []
     clear_v1() 
 
-def test_lists_a_single_channel(clear,auth_user_id):
-    channels_create_v1(auth_user_id, 'testChannel01', False)
-    returnDict = channels_list_v1(auth_user_id)
+def test_lists_a_single_channel(clear,token):
+    channels_create_v1(token, 'testChannel01', False)
+    returnDict = channels_list_v1(token)
     assert len(returnDict['channels']) == 1
     clear_v1() 
 
-def test_can_see_five_channels(clear, auth_user_id, names):
+def test_can_see_five_channels(clear, token, names):
     for name in names:
-        channels_create_v1(auth_user_id, name, False)
-    returnDict = channels_list_v1(auth_user_id)
+        channels_create_v1(token, name, False)
+    returnDict = channels_list_v1(token)
     assert len(returnDict['channels']) == 5
     clear_v1() 
 
-def test_can_only_see_one_of_six(clear, auth_user_id, names):
-    auth_user_id02 = auth_register_v1("test02@unsw.com", 'testPassword16', 'Test02', "User")['auth_user_id']
+def test_can_only_see_one_of_six(clear, token, names):
+    token2 = auth_register_v1("test02@unsw.com", 'testPassword16', 'Test02', "User")]
     for name in names:
-        channels_create_v1(auth_user_id, name, False)
-    channels_create_v1(auth_user_id02, 'testChannel06', False)
-    returnDict = channels_list_v1(auth_user_id02)
+        channels_create_v1(token, name, False)
+    channels_create_v1(token2, 'testChannel06', False)
+    returnDict = channels_list_v1(token2)
     for channel in returnDict['channels']:
         assert channel['name'] == 'testChannel06'
     clear_v1() 
         
-def test_invalid_authid(clear):
-    auth_user_id = 4
+def test_invalid_token(clear):
+    token = 4
     with pytest.raises(AccessError):
-        channels_list_v1(auth_user_id)
+        channels_list_v1(token)
     clear_v1() 
 
