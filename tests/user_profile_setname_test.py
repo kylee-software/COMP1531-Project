0dd6c48a89868e3248c229878e2bc344a15f7c2b
Sesi Adams <z5074999@ad.unsw.edu.au>
2021-04-02 18:39:03 +1100

    Updated functions from v1 to v2 - register/setname

--- tests/user_profile_setname_test.py
+++ tests/user_profile_setname_test.py
@@ -1,6 +1,6 @@
 import pytest
-from src.auth import auth_register_v1
-from src.user import user_profile_setname_v1
+from src.auth import auth_register_v2
+from src.user import user_profile_setname_v2
 from src.error import InputError
 from src.other import clear_v1
 from src.helper import find_user, load_data
@@ -10,7 +10,7 @@ def test_auth_user_id_valid():
     clear_v1()
 
     with pytest.raises(InputError):
-        user_profile_setname_v1(100, 'firstname', 'lastname')
+        user_profile_setname_v2(100, 'firstname', 'lastname')
 
     clear_v1()
 
@@ -18,10 +18,10 @@ def test_auth_user_id_valid():
 def test_first_name_incorrect_length():
     clear_v1()
 
-    user_1 = auth_register_v1('test@unsw.au', 'password', 'first', 'last')
+    user_1 = auth_register_v2('test@unsw.au', 'password', 'first', 'last')
 
     with pytest.raises(InputError):
-        user_profile_setname_v1(
+        user_profile_setname_v2(
             user_1['auth_user_id'], 'thisfirstnamecontainsspecialcharacters##^^&&**!!123123123', 'lastname')
 
     clear_v1()
@@ -30,10 +30,10 @@ def test_first_name_incorrect_length():
 def test_last_name_incorrect_length():
     clear_v1()
 
-    user_1 = auth_register_v1('test@unsw.au', 'password', 'first', 'last')
+    user_1 = auth_register_v2('test@unsw.au', 'password', 'first', 'last')
 
     with pytest.raises(InputError):
-        user_profile_setname_v1(user_1['auth_user_id'], 'firstname',
+        user_profile_setname_v2(user_1['auth_user_id'], 'firstname',
                                 'thislastnamecontainsspecialcharacters##^^&&**!!123123123')
 
     clear_v1()
@@ -42,8 +42,8 @@ def test_last_name_incorrect_length():
 def test_successful_setname():
     clear_v1()
 
-    user_1 = auth_register_v1('test@unsw.au', 'password', 'first', 'last')
-    user_profile_setname_v1(user_1['auth_user_id'], 'firstname', 'lastname')
+    user_1 = auth_register_v2('test@unsw.au', 'password', 'first', 'last')
+    user_profile_setname_v2(user_1['auth_user_id'], 'firstname', 'lastname')
 
     data = load_data()
     user_1_details = find_user(user_1['auth_user_id'], data)
