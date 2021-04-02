312837791e9cf2e9e9b6aa2cd1b1cc77430ec31d
Isabelle Ryan <z5205211@ad.unsw.edu.au>
2021-04-02 13:55:12 +1100

    Implemented tests for dm_remove

---
+++ src/dm.py
@@ -0,0 +1,8 @@
+from src.error import AccessError, InputError
+from src.helper import is_valid_user_id 
+from src.helper import is_valid_channel_id, load_data, save_data
+
+def dm_remove(token, dm_id):
+
+
+    return {}
