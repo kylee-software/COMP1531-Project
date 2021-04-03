1fd07e4a27c99c6d852f8efdad4dc0f0675c0fb1
Sesi Adams <z5074999@ad.unsw.edu.au>
2021-04-03 20:57:48 +1100

    Updated to pass pylint

--- tests/channel_addowner_v1_test.py
+++ tests/channel_addowner_v1_test.py
@@ -1,7 +1,7 @@
 import pytest
 from src.auth import auth_register_v2
 from src.channel import channel_addowner_v1
-from src.channels import channels_create_v1
+from src.channels import channels_create_v2
 from src.other import clear_v1
 from src.error import InputError, AccessError
 from src.helper import is_user_in_channel, load_data, find_user_channel_owner_status, save_data
@@ -22,7 +22,7 @@ def test_user_nonexistent():
     clear_v1()
 
     admin = auth_register_v2('test@unsw.au', 'password', 'first', 'last')
-    channel_id = channels_create_v1(admin['auth_user_id'], 'channel_1', True) 
+    channel_id = channels_create_v2(admin['auth_user_id'], 'channel_1', True)
     with pytest.raises(InputError):
         channel_addowner_v1(admin['auth_user_id'],
                             channel_id['channel_id'], 10)
@@ -33,7 +33,7 @@ def test_user_nonexistent():
 def test_user_already_owner():
     clear_v1()
     admin = auth_register_v2('test@unsw.au', 'password', 'first', 'last')
-    channel_id = channels_create_v1(admin['auth_user_id'], 'channel_1', True)
+    channel_id = channels_create_v2(admin['auth_user_id'], 'channel_1', True)
     with pytest.raises(InputError):
         channel_addowner_v1(admin['auth_user_id'],
                             channel_id['channel_id'], admin['auth_user_id'])
@@ -49,7 +49,7 @@ def test_not_owner_of_channel_or_dreams():
         'test2@unsw.au', 'password2', 'first2', 'last2')
     member_2 = auth_register_v2(
         'test3@unsw.au', 'password3', 'first3', 'last3')
-    channel_id = channels_create_v1(admin_1['auth_user_id'], 'channel_1', True)
+    channel_id = channels_create_v2(admin_1['auth_user_id'], 'channel_1', True)
     with pytest.raises(AccessError):
         channel_addowner_v1(member_1['auth_user_id'],
                             channel_id['channel_id'], member_2['auth_user_id'])
@@ -62,7 +62,7 @@ def test_successful_addowner():
 
     admin = auth_register_v2('test@unsw.au', 'password1', 'first1', 'last1')
     member = auth_register_v2('test1@unsw.au', 'password2', 'first2', 'last2')
-    channel_id = channels_create_v1(admin['auth_user_id'], 'channel_1', True)
+    channel_id = channels_create_v2(admin['auth_user_id'], 'channel_1', True)
     channel_addowner_v1(
         admin['auth_user_id'], channel_id['channel_id'], member['auth_user_id'])
 
