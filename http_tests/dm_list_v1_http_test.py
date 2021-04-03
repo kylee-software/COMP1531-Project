2c19b72664f3daf3c19aa8476cd835475f85ce47
Sesi Adams <z5074999@ad.unsw.edu.au>
2021-04-03 19:45:28 +1100

    Testing for dm/list/v1

---
+++ http_tests/dm_list_v1_http_test.py
@@ -0,0 +1,46 @@
+import requests
+from src import config
+from src.helper import is_valid_token, create_token
+
+
+def test_auth_user_id_nonexistent():
+    requests.delete(config.url + '/clear/v1')
+    invalid_token = create_token(1000, 100)
+    dm_list_call = requests.get(
+        config.url + '/dm/list/v1', json={'token': invalid_token})
+    assert dm_list_call.status_code == 403
+
+
+def test_success_case():
+    requests.delete(config.url + '/clear/v1')
+
+    admin = requests.post(config.url + '/admin/register/v2', json={
+                          'email': ' test1@unsw.au', 'password': 'password1', 'name_first': 'first1', 'name_last': 'last1'})
+    member_1 = requests.post(config.url + '/admin/register/v2', json={
+                             'email': ' test2@unsw.au', 'password': 'password2', 'name_first': 'first2', 'name_last': 'last2'})
+    member_2 = requests.post(config.url + '/admin/register/v2', json={
+                             'email': ' test3@unsw.au', 'password': 'password3', 'name_first': 'first3', 'name_last': 'last3'})
+
+    admin_details = admin.json()
+    member_1_details = member_1.json()
+    member_2_details = member_2.json()
+
+    member_list = []
+    member_list.append(member_1_details['user_id'])
+    member_list.append(member_2_details['user_id'])
+
+    requests.post(config.url + '/dm/create/v1',
+                  json={'token': admin_details['token'], 'u_ids': member_list})
+
+    member_1_dm = requests.get(
+        config.url + '/dm/list/v1', json={'token': member_1_details['token']})
+    member_2_dm = requests.get(
+        config.url + '/dm/list/v1', json={'token': member_2_details['token']})
+
+    member_1_dm_list = member_1_dm.json()
+    member_2_dm_list = member_2_dm.json()
+
+    assert member_1_dm.status_code == 200
+    assert member_2_dm.status_code == 200
+    assert len(member_1_dm_list) == 1
+    assert len(member_2_dm_list) == 1
---
+++ tests/dm_list_v1_test.py
@@ -0,0 +1,41 @@
+import pytest
+from src.other import clear_v1
+from src.error import AccessError
+from src.auth import auth_register_v2
+from src.dm import dm_list_v1, dm_create_v1
+from src.helper import create_token
+
+
+def test_token_user_nonexistent():
+    clear_v1()
+
+    invalid_token = create_token(100000, 1000)
+
+    with pytest.raises(AccessError):
+        dm_list_v1(invalid_token)
+
+    clear_v1()
+
+
+def test_success_case():
+    clear_v1()
+
+    admin = auth_register_v2('test1@unsw.au', 'password1', 'first1', 'last1')
+    member_1 = auth_register_v2(
+        'test2@unsw.au', 'password2', 'first2', 'last2')
+    member_2 = auth_register_v2(
+        'test3@unsw.au', 'password3', 'first3', 'last3')
+
+    members_list = []
+    members_list.append(member_1['auth_user_id'])
+    members_list.append(member_2['auth_user_id'])
+
+    dm_create_v1(admin['token'], members_list)
+
+    member_1_dm_list = dm_list_v1(member_1['token'])
+    member_2_dm_list = dm_list_v1(member_2['token'])
+
+    assert len(member_1_dm_list) == 1
+    assert len(member_2_dm_list) == 1
+
+    clear_v1()
