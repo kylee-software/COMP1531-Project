b59da384d910b0706cfe023491016a571e355b11
Kylee Bowers <47190841+kylee-software@users.noreply.github.com>
2021-04-05 05:29:05 -0500

    Testing removing channle messages and dm messages separately

--- tests/message_remove_v1_test.py
+++ tests/message_remove_v1_test.py
@@ -8,6 +8,15 @@ from src.message import message_senddm_v1, message_send_v2, message_remove_v1
 from src.error import InputError, AccessError
 
 @pytest.fixture
+def auth_user():
+    clear_v1()
+    email = "testmail@gamil.com"
+    password = "Testpass12345"
+    auth_user_info = auth_register_v2(email, password, "firstname", "lastname")
+    token = auth_user_info['token']
+    return token
+
+@pytest.fixture
 def member():
     email = "testmail1@gamil.com"
     password = "Testpass123456"
@@ -15,14 +24,6 @@ def member():
     last_name = "lastone"
     member = auth_register_v2(email, password, first_name, last_name)
     return member
-
-@pytest.fixture
-def auth_user():
-    email = "testmail@gamil.com"
-    password = "Testpass12345"
-    auth_user_info = auth_register_v2(email, password, "firstname", "lastname")
-    token = auth_user_info['token']
-    return token
 
 @pytest.fixture
 def channel_id(auth_user, member):
@@ -35,8 +36,7 @@ def dm_id(auth_user, member):
     dm_id = dm_create_v1(auth_user, [member['auth_user_id']])['dm_id']
     return dm_id
 
-def test_remove_messages(auth_user, channel_id, dm_id):
-    clear_v1()
+def test_remove_channel_message(auth_user, channel_id):
     message_send_v2(auth_user, channel_id, "Hi!")
     channel_messages_count_before = len(channel_messages_v2(auth_user, channel_id, 0)['messages'])
     assert channel_messages_count_before == 1
@@ -45,7 +45,7 @@ def test_remove_messages(auth_user, channel_id, dm_id):
     message_remove_v1(auth_user, channel_id)
     assert channel_messages_count_after == 0
 
-    clear_v1()
+def test_remove_dm_message(auth_user, dm_id):
     message_senddm_v1(auth_user, dm_id, "Hi!")
     dm_messages_count_before = len(dm_messages_v1(auth_user, dm_id, 0)['messages'])
     assert dm_messages_count_before == 1
@@ -54,13 +54,12 @@ def test_remove_messages(auth_user, channel_id, dm_id):
     dm_messages_count_after = len(dm_messages_v1(auth_user, dm_id, 0)['messages'])
     assert dm_messages_count_after == 0
 
-def test_invalid_token(dm_message_info):
-    clear_v1()
+def test_invalid_token(auth_user, channel_id):
+    channel_message_id = message_send_v2(auth_user, channel_id, "Hi!")
     with pytest.raises(AccessError):
-        message_remove_v1("invalid_token", dm_message_info[1])
+        message_remove_v1("invalid_token", channel_message_id)
 
 def test_unauthorised_auth_user(member, channel_id, dm_id):
-    clear_v1()
     # Test when a auth_user is not the sender nor an owner of the channel owner nor an owner of Dreams
     channel_message_id = message_send_v2(auth_user, channel_id, "Hi!")
     dm_message_id = message_senddm_v1(auth_user, dm_id, "Hi!")['message_id']
@@ -70,10 +69,6 @@ def test_unauthorised_auth_user(member, channel_id, dm_id):
         message_remove_v1(member['token'], dm_message_id)
 
 def test_invalid_message_id(auth_user):
-    clear_v1()
     with pytest.raises(InputError):
         message_remove_v1(auth_user, 1)
 
-    clear_v1()
-    with pytest.raises(InputError):
-        message_remove_v1(auth_user, 1)
