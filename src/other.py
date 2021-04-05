667514cca2679ad3e0a14aed65f699bb1dabb7b0
Sesi Adams <z5074999@ad.unsw.edu.au>
2021-04-06 04:42:34 +1000

    Implementation for search_v2

--- src/other.py
+++ src/other.py
@@ -10,15 +10,42 @@ def clear_v1():
 
 
 def search_v2(token, query_str):
+    decoded_token = is_valid_token(token)
+    if decoded_token is False:
+        raise AccessError(description='Not authorised to search.')
+
+    messages = []
+
+    data = load_data()
+
+    for channel in data['channels']:
+        is_in_channel = False
+        for member in channel['members']:
+            if member['user_id'] == decoded_token['auth_user_id']:
+                is_in_channel = True
+                break
+        if decoded_token['auth_user_id'] in channel['owner']:
+            is_in_channel = True
+        if is_in_channel:
+            for channel_message in channel['messages']:
+                if query_str in channel_message:
+                    messages.append(channel_message)
+            break
+
+    for dm in data['dms']:
+        is_in_dm = False
+        if decoded_token['auth_user_id'] in dm['members']:
+            is_in_dm = True
+        if dm['creator'] == decoded_token['auth_user_id']:
+            is_in_dm = True
+            break
+        if is_in_dm:
+            for dm_message in dm['messages']:
+                if query_str in dm_message:
+                    messages.append(dm_message)
+            break
     return {
-        'messages': [
-            {
-                'message_id': 1,
-                'u_id': 1,
-                'message': 'Hello world',
-                'time_created': 1582426789,
-            }
-        ],
+        'messages': messages
     }
 
 
