286b73aa9d0533d46326d0ec02befdeb5c842e7d
Isabelle Ryan <z5205211@ad.unsw.edu.au>
2021-04-05 19:04:21 +1000

    added import back in to sever.py so all works again

--- src/server.py
+++ src/server.py
@@ -10,7 +10,7 @@ from src import config
 from src.channel import channel_details_v1, channel_join_v1, channel_invite_v1, channel_leave_v1, channel_messages_v2
 from src.other import clear_v1
 from src.channels import channels_create_v2, channels_listall_v2, channels_list_v2
-from src.user import user_profile_v2
+from src.user import user_profile_v2, user_profile_sethandle_v1
 from src.auth import auth_login_v2, auth_register_v2, auth_logout_v1
 from src.message import message_send_v2, message_senddm_v1
 
