c36cda96e9f25c7e9ace5041ea85f8fe01ed31e8
Kylee Bowers <47190841+kylee-software@users.noreply.github.com>
2021-04-05 01:41:13 -0500

    Added helper functiond that didn't merged to the master

--- src/helper.py
+++ src/helper.py
@@ -5,6 +5,7 @@ import json
 
 SECRET = 'WED09B-ECHO'
 
+
 def return_valid_tagged_handles(message, channel_id):
     data = load_data()
     split_message = message.split()
@@ -27,6 +28,7 @@ def return_valid_tagged_handles(message, channel_id):
                 real_handles_in_channel.append(handle)
 
     return real_handles_in_channel
+
 
 def is_valid_user_id(auth_user_id):
     '''
@@ -66,11 +68,13 @@ def is_valid_channel_id(channel_id):
             return True
     return False
 
+
 def is_valid_dm_id(dm_id, data):
     for dm in data['dms']:
         if dm['dm_id'] == dm_id:
             return True
     return False
+
 
 def hash_password(password):
     '''
@@ -174,10 +178,12 @@ def find_channel(channel_id, data):
         if channel['channel_id'] == channel_id:
             return channel
 
+
 def find_dm(dm_id, data):
     for dm in data['dms']:
         if dm['dm_id'] == dm_id:
             return dm
+
 
 def is_user_in_channel(channel_id, user_id, data):
     channel = find_channel(channel_id, data)
@@ -196,29 +202,32 @@ def is_user_in_dm(dm_id, user_id, data):
         return True
     return False
 
+
 def find_message_source(message_id, data):
     for channel in data['channels']:
         for message in channel['messages']:
             if message['message_id'] == message_id:
                 return channel
-    
+
     for dm in data['dms']:
         for message in dm['messages']:
             if message['message_id'] == message_id:
                 return dm
     return None
 
+
 def find_message(message_id, data):
     for channel in data['channels']:
         for message in channel['messages']:
             if message['message_id'] == message_id:
                 return message['message']
-    
+
     for dm in data['dms']:
         for message in dm['messages']:
             if message['message_id'] == message_id:
                 return message['message']
     return ""
+
 
 def tag_users(message, sender_handle, dm_id, channel_id):
     data = load_data()
@@ -227,9 +236,9 @@ def tag_users(message, sender_handle, dm_id, channel_id):
     for word in split_message:
         if word.startswith('@'):
             tagged_handles.append(word.strip('@'))
-    
+
     users_tagged = []
-    for handle in tagged_handles:    
+    for handle in tagged_handles:
         for user in data['users']:
             if handle == user['account_handle']:
                 users_tagged.append(user)
@@ -240,7 +249,8 @@ def tag_users(message, sender_handle, dm_id, channel_id):
             for user in users_tagged:
                 if user['user_id'] in dm['members']:
                     notification_message = f"{sender_handle} tagged you in {dm['name']}: {message[:20]}"
-                    user['notifications'].insert(0, {'channel_id' : -1, 'dm_id': dm_id, 'notification_message': notification_message})
+                    user['notifications'].insert(0, {'channel_id': -1, 'dm_id': dm_id,
+                                                     'notification_message': notification_message})
 
     if channel_id != -1:
         if is_valid_channel_id(channel_id) == True:
@@ -252,7 +262,7 @@ def tag_users(message, sender_handle, dm_id, channel_id):
             for user in users_tagged:
                 if user['user_id'] in members:
                     notification_message = f"{sender_handle} tagged you in {channel['name']}: {message[:20]}"
-                    user['notifications'].insert(0, {'channel_id' : channel_id, 'dm_id': -1, 'notification_message': notification_message})
-        
-    return None
+                    user['notifications'].insert(0, {'channel_id': channel_id, 'dm_id': -1,
+                                                     'notification_message': notification_message})
 
+    return None
