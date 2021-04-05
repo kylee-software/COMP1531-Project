e96a91af5f759ea8e141e06914ba1790000c5a92
Isabelle Ryan <z5205211@ad.unsw.edu.au>
2021-04-05 13:57:08 +1000

    minor fix in http test so it all works now - used params={} instead of json={}

--- http_tests/user_sethandle_http_test.py
+++ http_tests/user_sethandle_http_test.py
@@ -21,14 +21,14 @@ def test_user_profile_sethandle(clear, user1):
     '''
     A simple test to check user profile sethandle
     '''
-    resp = requests.put(config.url + "user/profile/sethandle/v1", params={'token': user1['token'], 'handle_str':"newhandle"})
+    resp = requests.put(config.url + "user/profile/sethandle/v1", json={'token': user1['token'], 'handle_str':"newhandle"})
     assert resp.json() == {}
 
 def test_user_sethandle_access_error(clear):
-    resp = requests.put(config.url + "user/profile/sethandle/v1", params={'token': 'invalid.token.input', 'handle_str':"newhandle"})
+    resp = requests.put(config.url + "user/profile/sethandle/v1", json={'token': 'invalid.token.input', 'handle_str':"newhandle"})
     assert resp.status_code == 403
 
 def test_user_sethandel_input_error(clear, user1):
-    resp = requests.put(config.url + "user/profile/sethandle/v1", params={'token': user1['token'], 'handle_str':"newhandle"*1000})    
+    resp = requests.put(config.url + "user/profile/sethandle/v1", json={'token': user1['token'], 'handle_str':"newhandle"*1000})    
     assert resp.status_code == 400
 
