c5335faf2437c26375d3f307ca4837101004a51a
Sesi Adams <z5074999@ad.unsw.edu.au>
2021-04-05 23:13:40 +1000

    Added tests

--- http_tests/message_edit_v2_http_test.py
+++ http_tests/message_edit_v2_http_test.py
@@ -40,12 +40,12 @@ def channel(admin):
 
 @ pytest.fixture
 def dm(admin, member):
-    return requests.post(config.url + '/dm/create/v1', json={'token': admin['token'], 'u_ids': member['auth_user_id']}).json()
+    return requests.post(config.url + '/dm/create/v1', json={'token': admin['token'], 'u_ids': [member['auth_user_id']]}).json()
 
 
 @ pytest.fixture
 def channel_message(admin, channel):
-    return requests.post(config.url + '/message/send/v2', json={admin['token'], 'channel_id': ['channel_id'], 'message': 'this is a message sent to the other user in the channel.'}).json()
+    return requests.post(config.url + '/message/send/v2', json={'token': admin['token'], 'channel_id': channel['channel_id'], 'message': 'this is a message sent to the other user in the channel.'}).json()
 
 
 @ pytest.fixture
@@ -56,48 +56,48 @@ def dm_message(admin, dm):
 def test_invalid_token_dm(dm_message):
     invalid_token = 'invalidtoken123123'
     message_call = requests.put(config.url + '/message/edit/v2', json={
-        'token': invalid_token, 'message_id': dm_message['message_id'], 'message': 'this is an updated message in the dm.'})
+        'token': invalid_token, 'message_id': dm_message, 'message': 'this is an updated message in the dm.'})
     assert message_call.status_code == 403
 
 
 def test_invalid_token_channel(channel_message):
     invalid_token = 'invalidtoken123123'
     message_call = requests.put(config.url + '/message/edit/v2', json={
-        'token': invalid_token, 'message_id': channel_message['message_id'], 'message': 'this is an updated message in the dm.'})
+        'token': invalid_token, 'message_id': channel_message, 'message': 'this is an updated message in the dm.'})
     assert message_call.status_code == 403
 
 
 def test_message_incorrect_length_dm(admin, dm_message):
     message_call = requests.put(config.url + '/message/edit/v2', json={
-        'token': admin['token'], 'message_id': dm_message['message_id'], 'message': 1500*'A'})
+        'token': admin['token'], 'message_id': dm_message, 'message': 1500*'A'})
     assert message_call.status_code == 400
 
 
 def test_message_incorrect_length_channel(admin, channel_message):
     message_call = requests.put(config.url + '/message/edit/v2', json={
-        'token': admin['token'], 'message_id': channel_message['message_id'], 'message': 1500*'A'})
+        'token': admin['token'], 'message_id': channel_message, 'message': 1500*'A'})
     assert message_call.status_code == 400
 
 
-def test_message_sent_by_unauthorised_user_and_not_channel_owner(member, channel_message):
-    message_call = requests.put(config.url + '/message/edit/v2', json={'token': member['token'], 'message_id': channel_message['message_id'],
+def test_message_sent_by_unauthorised_user_and_not_channel_owner(admin, member, channel_message):
+    message_call = requests.put(config.url + '/message/edit/v2', json={'token': member['token'], 'message_id': channel_message,
                                                                        'message': 'this is an updated message in the dm.'})
-    assert message_call.status_code == 400
+    assert message_call.status_code == 403
 
 
 def test_success_channel_message(admin, channel, channel_message):
-    edit = requests.put(config.url + '/message/edit/v2', json={'token': admin['token'], 'message_id': channel_message['message_id'],
+    edit = requests.put(config.url + '/message/edit/v2', json={'token': admin['token'], 'message_id': channel_message,
                                                                'message': 'this edit is valid in this channel.'})
-    channel_messages = requests.get(config.url + '/channel/messages/v2', json={
-        'token': admin['token'], 'message_id': channel['channel_id'], 'start': 0}).json
-    assert channel_messages['messages'][0] == 'this edit is valid in this channel.'
+    channel_messages = requests.get(config.url + '/channel/messages/v2', params={
+        'token': admin['token'], 'channel_id': channel['channel_id'], 'start': 0}).json()
+    assert channel_messages['messages'][0]['message'] == 'this edit is valid in this channel.'
     assert edit.status_code == 200
 
 
 def test_success_dm_message(admin, dm, dm_message):
     edit = requests.put(config.url + '/message/edit/v2', json={'token': admin['token'], 'message_id': dm_message['message_id'],
-                                                               'message': 'this edit is valid in this dm'})
-    dm_messages = requests.get(config.url + '/dm/messages/v1', json={
-                               'token': admin['token'], 'message_id': dm['dm_id'], 'start': 0})
-    assert dm_messages['messages'][0] == 'this edit is valid in this dm.'
+                                                               'message': 'this edit is valid in this dm.'})
+    dm_messages = requests.get(config.url + '/dm/messages/v1', params={
+                               'token': admin['token'], 'dm_id': dm['dm_id'], 'start': 0}).json()
+    assert dm_messages['messages'][0]['message'] == 'this edit is valid in this dm.'
     assert edit.status_code == 200
--- tests/message_edit_v2_test.py
+++ tests/message_edit_v2_test.py
@@ -57,7 +57,7 @@ def test_invalid_token_channel(channel_message):
     invalid_token = 'invalidtoken123123'
     with pytest.raises(AccessError):
         message_edit_v2(
-            invalid_token, channel_message['message_id'], 'this is an updated message in the dm.')
+            invalid_token, channel_message, 'this is an updated message in the dm.')
 
 
 def test_message_incorrect_length_dm(admin, dm_message):
@@ -68,25 +68,25 @@ def test_message_incorrect_length_dm(admin, dm_message):
 def test_message_incorrect_length_channel(admin, channel_message):
     with pytest.raises(InputError):
         message_edit_v2(
-            admin['token'], channel_message['message_id'], 1500*'A')
+            admin['token'], channel_message, 1500*'A')
 
 
-def test_message_sent_by_unauthorised_user_and_not_channel_owner(member, channel_message):
+def test_message_sent_by_unauthorised_user_and_not_channel_owner(admin, member, channel_message):
     with pytest.raises(AccessError):
-        message_edit_v2(member['token'], channel_message['message_id'],
+        message_edit_v2(member['token'], channel_message,
                         'this is an updated message in the dm.')
 
 
 def test_success_channel_message(admin, channel, channel_message):
-    message_edit_v2(admin['token'], channel_message['message_id'],
+    message_edit_v2(admin['token'], channel_message,
                     'this edit is valid in this channel.')
     channel_messages = channel_messages_v2(
         admin['token'], channel['channel_id'], 0)
-    assert channel_messages['messages'][0] == 'this edit is valid in this channel.'
+    assert channel_messages['messages'][0]['message'] == 'this edit is valid in this channel.'
 
 
 def test_success_dm_message(admin, dm, dm_message):
     message_edit_v2(admin['token'], dm_message['message_id'],
-                    'this edit is valid in this dm')
+                    'this edit is valid in this dm.')
     dm_messages = dm_messages_v1(admin['token'], dm['dm_id'], 0)
-    assert dm_messages['messages'][0] == 'this edit is valid in this dm.'
+    assert dm_messages['messages'][0]['message'] == 'this edit is valid in this dm.'
