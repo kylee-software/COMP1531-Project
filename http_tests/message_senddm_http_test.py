9f1cdffb294de557d8d292d6b2c10992fe7bc842
Isabelle Ryan <z5205211@ad.unsw.edu.au>
2021-04-05 13:22:41 +1000

    http tests working - waiting on dm messages for more thorough testing

--- http_tests/message_senddm_http_test.py
+++ http_tests/message_senddm_http_test.py
@@ -48,14 +48,14 @@ def test_message_senddm(clear, dm):
     user2 = auth_login_v2("test2email@gmail.com", "TestTest2")
     #message_senddm_v1(user2['token'], dm['dm_id'], "message")
     resp = requests.post(config.url + 'message/senddm/v1', json={'token': user2['token'], 'dm_id':dm['dm_id'], 'message':'messagemessage'})
-    assert resp.json() == {}
+    assert isinstance(resp.json()['message_id'], int)
 
 def test_message_senddm_access(clear, dm, user1):
     '''
     A simple test to check message send dm access error
     '''
     resp = requests.post(config.url + 'message/senddm/v1', json={'token': user1['token'], 'dm_id':dm['dm_id'], 'message':'messagemessage'})
-    assert resp.status_code == 400
+    assert resp.status_code == 403
 
 def test_message_senddm_input(clear, dm):
     '''
@@ -63,4 +63,4 @@ def test_message_senddm_input(clear, dm):
     '''
     user2 = auth_login_v2("test2email@gmail.com", "TestTest2")
     resp = requests.post(config.url + 'message/senddm/v1', json={'token': user2['token'], 'dm_id':dm['dm_id'], 'message':'messagelong'*500})
-    assert resp.status_code == 200
+    assert resp.status_code == 400
