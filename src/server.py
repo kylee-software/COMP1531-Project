c2407166e031558f2deaa672b8bbeaff5920b52a
Z5304138 <k.bowers@student.unsw.edu.au>
2021-04-16 16:08:30 -0500

    Created a route for message_unreact()

--- src/server.py
+++ src/server.py
@@ -18,7 +18,7 @@ from src.dm import (dm_create_v1, dm_details_v1, dm_invite_v1, dm_leave_v1,
                     dm_list_v1, dm_messages_v1, dm_remove_v1)
 from src.error import AccessError, InputError
 from src.message import (message_edit_v2, message_remove_v1, message_send_v2,
-                         message_senddm_v1, message_share_v1, message_react_v1)
+                         message_senddm_v1, message_share_v1, message_react_v1, message_unreact_v1)
 from src.other import clear_v1, notifications_get_v1, search_v2
 from src.user import (user_profile_setemail_v2, user_profile_sethandle_v1,
                       user_profile_setname_v2, user_profile_v2, users_all_v1)
@@ -319,6 +319,12 @@ def search():
 def message_react():
     data = request.get_json()
     message_react_v1(data['token'], data['message_id'], data['react_id'])
+    return jsonify({})
+
+@APP.route("/message/unreact/v1", methods=['POST'])
+def message_unreact():
+    data = request.get_json()
+    message_unreact_v1(data['token'], data['message_id'], data['react_id'])
     return jsonify({})
 
 if __name__ == "__main__":
